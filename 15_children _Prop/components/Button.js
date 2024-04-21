42bcd3f8d8ccb9e7f28c3712b8ba6b29480750d0
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-21 21:35:08 +0530

    Button js

--- 15_children _Prop/components/Button.js
+++ 15_children _Prop/components/Button.js
@@ -1,6 +1,8 @@
-const Button = ({ imageURL, imageText, onClick }) => {
+const Button = ({ imageURL, imageText, onClick, children }) => {
+  console.log(children)
   return (
     <button title={imageText} onClick={onClick}>
+      {imageText}
       <img src={imageURL} alt={imageText} />
     </button>
   );
