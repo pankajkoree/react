d87484eb50ee8025cc6b8287e50c6f25ee5a7b8c
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-22 15:03:23 +0530

    counter.js

--- 17_scopeCSS_inReact/components/Counter.js
+++ 17_scopeCSS_inReact/components/Counter.js
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import styles from "./Counter.module.css";
 
-export const Counter = ({ counterName, children: AppleCounter}) => {
+
+
+export const Counter = ({ counterName, children: AppleCounter }) => {
   const [count, setCount] = useState(0);
   console.log(AppleCounter);
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>{count}</h1>
+      <h1 className={styles['text-XL']}>{count}</h1>
       <button
+        className={[styles.button, styles.textXL].join(' ')}
         onClick={() => {
           setCount((previousState) => previousState + 1);
           setCount((previousState) => previousState + 1);
