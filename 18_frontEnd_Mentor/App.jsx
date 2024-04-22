3394dca4fb3c3b459bca20f9ff2ab7f32b87f66b
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-22 23:47:46 +0530

    project

--- 18_frontEnd_Mentor/App.jsx
+++ 18_frontEnd_Mentor/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./components/Header";
 import "./App.css";
+import SearchBar from "./components/SearchBar";
+import SelectMenu from "./components/SelectMenu";
 const App = () => {
   return (
     <>
       <Header />
+      <div className="search-filter-container">
+        <SearchBar />
+        <SelectMenu />
+      </div>
     </>
   );
 };
