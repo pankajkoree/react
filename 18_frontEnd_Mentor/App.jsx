8eaa2d2734259f59ac1b2fbf1733bbb892d4a181
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-23 22:49:13 +0530

    app.jsx

--- 18_frontEnd_Mentor/App.jsx
+++ 18_frontEnd_Mentor/App.jsx
@@ -1,19 +1,21 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import SelectMenu from "./components/SelectMenu";
 import CountriesList from "./components/CountriesList";
-
 import "./App.css";
+
 const App = () => {
+  const [query, setQuery] = useState("");
   return (
     <>
       <Header />
       <main>
         <div className="search-filter-container">
-          <SearchBar />
+          <SearchBar setQuery={setQuery} />
           <SelectMenu />
         </div>
-        <CountriesList />
+        <CountriesList query={query} />
       </main>
     </>
   );
