41473cf5b376a86eeb61dca2aaa618c67a8274a0
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-23 10:34:30 +0530

    card making for country

--- 18_frontEnd_Mentor/components/CountryCard.jsx
+++ 18_frontEnd_Mentor/components/CountryCard.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 
-export default function CountryCard() {
+export default function CountryCard({
+  name,
+  flag,
+  capital,
+  population,
+  region,
+}) {
   return (
     <a className="country-card" href="/country-html?name=Barbados">
-      <img src="https://flagcdn.com/bb.svg" alt="Barbados flag" />
+      <img src={flag} alt={name + "flag"} />
       <div className="card-text">
-        <h3 className="card-title">Brabados</h3>
+        <h3 className="card-title">{name}</h3>
         <p>
-          <b>Population: </b>2,87,371
+          <b>Population: </b>
+          {population.toLocaleString("en-IN")}
         </p>
         <p>
-          <b>Region: </b>Americas
+          <b>Region: </b>
+          {region}
         </p>
         <p>
-          <b>Capital: </b>Bridgetown
+          <b>Capital: </b>
+          {capital}
         </p>
       </div>
     </a>
