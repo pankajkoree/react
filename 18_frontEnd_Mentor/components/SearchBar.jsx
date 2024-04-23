4f060b7bb67151b6fcbe7cd7397033a4407d97f1
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-23 22:50:41 +0530

    searchbar.jsx

--- 18_frontEnd_Mentor/components/SearchBar.jsx
+++ 18_frontEnd_Mentor/components/SearchBar.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
 
-export default function SearchBar() {
+export default function SearchBar({setQuery}) {
   return (
-    <div className='search-container'>
-        <i className='fa-solid fa-magnifying-glass'></i>
-        <input type="text" placeholder='Search for a country...' />
+    <div className="search-container">
+      <i className="fa-solid fa-magnifying-glass"></i>
+      <input
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+        type="text"
+        placeholder="Search for a country..."
+      />
     </div>
-  )
+  );
 }
