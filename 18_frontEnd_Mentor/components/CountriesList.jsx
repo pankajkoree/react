9104cf33b84b17e24cc91efbecff3ce5d402a2e0
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-23 22:36:56 +0530

    countries list jsx

--- 18_frontEnd_Mentor/components/CountriesList.jsx
+++ 18_frontEnd_Mentor/components/CountriesList.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import countriesData from "../countriesData";
 import CountryCard from "./CountryCard";
+import { useState } from "react";
 
 export default function CountriesList() {
-  const array = countriesData.map((country, i) => {
-    return (
-      <CountryCard
-        key = {country.name.common}
-        name={country.name.common}
-        flag={country.flags.svg}
-        capital={country.capital?.[0]}
-        population={country.population}
-        region={country.region}
-      />
-    );
-  });
-  return <div className="countries-container">{array}</div>;
+  const [query, setQuery] = useState("");
+  
+  return (
+    <>
+    <input type="text" onChange={(e) => setQuery(e.target.value.toLowerCase())} />
+    <div className="countries-container">
+      {countriesData.filter((country) => country.name.common.toLowerCase().includes(query)).map((country) => {
+        return (
+          <CountryCard
+            key={country.name.common}
+            name={country.name.common}
+            flag={country.flags.svg}
+            population={country.population}
+            region={country.region}
+            capital={country.capital?.[0]}
+          />
+        );
+      })}
+    </div>
+    </>
+    
+  );
 }
