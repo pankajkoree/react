761736559a0906034d2d8340ebdd32a876e33af9
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-24 18:52:47 +0530

    app.jsx

--- 20_routing_inProjects/App.jsx
+++ 20_routing_inProjects/App.jsx
@@ -1,22 +1,15 @@
 import { useState } from "react";
-import Header from "./components/Header";
-import SearchBar from "./components/SearchBar";
-import SelectMenu from "./components/SelectMenu";
-import CountriesList from "./components/CountriesList";
+import Header from './components/Header'
+
 import "./App.css";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   const [query, setQuery] = useState("");
   return (
     <>
-      <Header />
-      <main>
-        <div className="search-filter-container">
-          <SearchBar setQuery={setQuery} />
-          <SelectMenu />
-        </div>
-        {query === "unmount" ? "" : <CountriesList query={query} />}
-      </main>
+     <Header />
+      <Outlet />
     </>
   );
 };
