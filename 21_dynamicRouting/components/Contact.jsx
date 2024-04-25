662a16ad9cd16db94b89998e8e9500013141defe
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-25 12:07:45 +0530

    contact.jsx

--- 21_dynamicRouting/components/Contact.jsx
+++ 21_dynamicRouting/components/Contact.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function () {
+  const params = useParams();
+  console.log(params)
   return (
     <div>
       <h1>contact us</h1>
