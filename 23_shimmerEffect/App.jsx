ff35eb24b7fc42240046fcc895b740ec20f64864
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-26 23:06:01 +0530

    App.jsx

--- 23_shimmerEffect/App.jsx
+++ 23_shimmerEffect/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
-import Header from './components/Header'
+import Header from "./components/Header";
 
 import "./App.css";
 import { Outlet } from "react-router-dom";
 
 const App = () => {
-  const [query, setQuery] = useState("");
+  // const [query, setQuery] = useState("");
+  const [isDark, setIsDark] = useState(
+    JSON.parse(localStorage.getItem("isDarkMode"))
+  );
   return (
     <>
-     <Header />
-      <Outlet />
+      <Header theme={[isDark, setIsDark]} />
+      <Outlet context={[isDark, setIsDark]} />
     </>
   );
 };
