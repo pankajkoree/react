4e3a6cb05aa66e1239478ecc2945240ecbaa2d5a
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-26 23:06:52 +0530

    Header.jsx

--- 23_shimmerEffect/components/Header.jsx
+++ 23_shimmerEffect/components/Header.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const Header = () => {
+export const Header = ({ theme }) => {
+  const [isDark, setIsDark] = theme;
+  // if (isDark) {
+  //   document.body.classList.add("dark");
+  // } else {
+  //   document.body.classList.remove("dark");
+  // }
   return (
-    <header className="header-container">
+    <header className={`header - container ${isDark ? "dark" : ""}`}>
       <div className="header-content">
         <h2 className="title">
           <a href="/">Where in the world?</a>
         </h2>
-        <p className="theme-changer">
-          <i className="fa-regular fa-moon"></i>&nbsp;&nbsp; Dark Mode
+        <p
+          className="theme-changer"
+          onClick={() => {
+            setIsDark(!isDark);
+            localStorage.setItem("isDarkMode", !isDark);
+          }}
+        >
+          <i className={`fa-solid fa-${isDark ? "sun" : "moon"}`}></i>
+          &nbsp;&nbsp; {isDark ? "Light" : "Dark"} Mode
         </p>
       </div>
     </header>
