12e4c5fdd7c85fe9758f9bb783977acf4c49e922
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-27 18:52:38 +0530

    Home.jsx

--- 24_contextAPI/components/Home.jsx
+++ 24_contextAPI/components/Home.jsx
@@ -1,21 +1,20 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 import SelectMenu from "./SelectMenu";
 import CountriesList from "./CountriesList";
 import "../App.css";
-import { useOutletContext } from "react-router-dom";
-import { themeContext } from "../contexts/ThemeContext";
+import { useTheme } from "../hooks/useTheme";
 
 export default function Home() {
   const [query, setQuery] = useState("");
-  const [isDark] = useContext(themeContext);
+  const [isDark] = useTheme();
   return (
     <main className={`${isDark ? "dark" : ""}`}>
       <div className="search-filter-container">
         <SearchBar setQuery={setQuery} />
         <SelectMenu />
       </div>
-      {query === "unmount" ? "" : <CountriesList query={query} />}
+      {<CountriesList query={query} />}
     </main>
   );
 }
