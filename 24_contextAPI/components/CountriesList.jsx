3fbf41779da1a6483619a491cab7d8a9b1b5c585
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-27 11:42:12 +0530

    CountriesList.jsx

--- 24_contextAPI/components/CountriesList.jsx
+++ 24_contextAPI/components/CountriesList.jsx
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from "react";
-// import countriesData from "../countriesData";
+// import countriesData from '../countriesData'
 import CountryCard from "./CountryCard";
 import CountriesListShimmer from "./CountriesListShimmer";
 
 export default function CountriesList({ query }) {
   const [countriesData, setCountriesData] = useState([]);
-  // useEffect enables to run a things only once when array is empty, used to monitor when state changes
+
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
       .then((data) => {
         setCountriesData(data);
       });
-  },[]);
-  if (countriesData.length === 0) {
+  }, []);
+
+  if (!countriesData.length) {
     return <CountriesListShimmer />;
   }
 
@@ -21,9 +22,8 @@ export default function CountriesList({ query }) {
     <>
       <div className="countries-container">
         {countriesData
-          .filter(
-            (country) =>
-              country.name && country.name.common.toLowerCase().includes(query)
+          .filter((country) =>
+            country.name.common.toLowerCase().includes(query)
           )
           .map((country) => {
             return (
@@ -39,31 +39,6 @@ export default function CountriesList({ query }) {
             );
           })}
       </div>
-
-      {/* {!countriesData.length ? (
-        <CountriesListShimmer />
-      ) : (
-        <div className="countries-container">
-          {countriesData
-            .filter(
-              (country) =>
-                country.name &&
-                country.name.common.toLowerCase().includes(query)
-            )
-            .map((country) => {
-              return (
-                <CountryCard
-                  key={country.name.common}
-                  name={country.name.common}
-                  flag={country.flags.svg}
-                  population={country.population}
-                  region={country.region}
-                  capital={country.capital?.[0]}
-                />
-              );
-            })}
-        </div>
-      )} */}
     </>
   );
 }
