0be1754e80f924598ee464f54350f2ea8d7a4783
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-27 11:39:24 +0530

    Home.jsx

--- 24_contextAPI/components/Home.jsx
+++ 24_contextAPI/components/Home.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import SearchBar from "./SearchBar";
 import SelectMenu from "./SelectMenu";
 import CountriesList from "./CountriesList";
 import "../App.css";
 import { useOutletContext } from "react-router-dom";
+import { themeContext } from "../contexts/ThemeContext";
 
 export default function Home() {
   const [query, setQuery] = useState("");
-  const [isDark] = useOutletContext();
+  const [isDark] = useContext(themeContext);
   return (
-    <main className={`${isDark? "dark": "" }`}>
+    <main className={`${isDark ? "dark" : ""}`}>
       <div className="search-filter-container">
         <SearchBar setQuery={setQuery} />
         <SelectMenu />
