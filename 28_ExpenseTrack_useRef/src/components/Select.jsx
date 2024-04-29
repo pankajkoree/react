3bd4de46e49afe3adaa0657a0b8f96991be78413
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-29 21:13:02 +0530

    src/components/Select.jsx

--- 28_ExpenseTrack_useRef/src/components/Select.jsx
+++ 28_ExpenseTrack_useRef/src/components/Select.jsx
@@ -1,18 +1,29 @@
 import PropTypes from "prop-types";
 
-export default function Select({ label, id, name, value, onChange, error }) {
+export default function Select({
+  label,
+  id,
+  name,
+  value,
+  onChange,
+  error,
+  options,
+  defaultOption,
+}) {
   return (
     <div className="input-container">
       <label htmlFor={id}>{label}</label>
       <select id={id} name={name} value={value} onChange={onChange}>
-        <option value="" hidden>
-          Select Category
-        </option>
-        <option value="Grocery">Grocery</option>
-        <option value="Clothes">Clothes</option>
-        <option value="Bills">Bills</option>
-        <option value="Education">Education</option>
-        <option value="Medicine">Medicine</option>
+        {defaultOption && (
+          <option value="" hidden>
+            {defaultOption}
+          </option>
+        )}
+        {options.map((option, i) => (
+          <option key={i} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
       <p className="error">{error}</p>
     </div>
@@ -26,4 +37,6 @@ Select.propTypes = {
   value: PropTypes.string,
   onChange: PropTypes.func.isRequired,
   error: PropTypes.node,
+  options: PropTypes.array,
+  defaultOption: PropTypes.string,
 };
