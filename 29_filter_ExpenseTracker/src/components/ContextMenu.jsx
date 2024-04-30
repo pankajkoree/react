1bfd0a422f198067bd39aefd6e482002cbc13f64
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-30 22:11:42 +0530

    src/components/ContextMenu.jsx

--- 29_filter_ExpenseTracker/src/components/ContextMenu.jsx
+++ 29_filter_ExpenseTracker/src/components/ContextMenu.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-
-export default function () {
+export default function ContextMenu(menuPosition) {
+  if (!menuPosition.left) return;
   return (
-    <div className="context-menu">
+    <div className="context-menu" style={menuPosition}>
       <div>Edit</div>
       <div>Delete</div>
     </div>
