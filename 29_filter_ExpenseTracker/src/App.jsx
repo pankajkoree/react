aeddba6462b00cf5747cf8e569da4212b0f3b8ad
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-05-01 23:19:23 +0530

    src/App.jsx

--- 29_filter_ExpenseTracker/src/App.jsx
+++ 29_filter_ExpenseTracker/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseTable from "./components/ExpenseTable";
-import expenseData from "./components/expenseData";
+import expenseData from "./components/expenseData.js";
 
 function App() {
   const [expense, setExpense] = useState({
@@ -11,28 +11,26 @@ function App() {
     amount: "",
   });
   const [expenses, setExpenses] = useState(expenseData);
-  const [editingRowId, setEditingRowId] = useState();
+  const [editingRowId, setEditingRowId] = useState("");
 
   return (
     <main>
-      <>
-        <h1>Track Your Expense</h1>
-        <div className="expense-tracker">
-          <ExpenseForm
-            setExpenses={setExpenses}
-            expense={expense}
-            setExpense={setExpense}
-            editingRowId={editingRowId}
-            setEditingRowId={setEditingRowId}
-          />
-          <ExpenseTable
-            expenses={expenses}
-            setExpense={setExpense}
-            setExpenses={setExpenses}
-            setEditingRowId={setEditingRowId}
-          />
-        </div>
-      </>
+      <h1>Track Your Expense</h1>
+      <div className="expense-tracker">
+        <ExpenseForm
+          setExpenses={setExpenses}
+          expense={expense}
+          setExpense={setExpense}
+          editingRowId={editingRowId}
+          setEditingRowId={setEditingRowId}
+        />
+        <ExpenseTable
+          expenses={expenses}
+          setExpense={setExpense}
+          setExpenses={setExpenses}
+          setEditingRowId={setEditingRowId}
+        />
+      </div>
     </main>
   );
 }
