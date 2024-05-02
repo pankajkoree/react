15446ce6da5f8f5e7b97b580b76a2bbace4402e5
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-05-02 14:13:24 +0530

    App.jsx

--- 30_sorting_ExpenseTracker/src/App.jsx
+++ 30_sorting_ExpenseTracker/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseTable from "./components/ExpenseTable";
 import expenseData from "./components/expenseData.js";
+import { useLocalStorage } from "./hooks/useLocalStorage.js";
 
 function App() {
   const [expense, setExpense] = useState({
@@ -12,10 +13,19 @@ function App() {
   });
   const [expenses, setExpenses] = useState(expenseData);
   const [editingRowId, setEditingRowId] = useState("");
+  const [localData, setLocalData] = useLocalStorage("myNum", [1, 2, 3]);
+  console.log(localData);
 
   return (
     <main>
-      <h1>Track Your Expense</h1>
+      <h1
+        onClick={() => {
+          setLocalData([4, 5, 6]);
+        }}
+      >
+        Track Your Expense
+      </h1>
+      <h2>{localData.join(", ")}</h2>
       <div className="expense-tracker">
         <ExpenseForm
           setExpenses={setExpenses}
