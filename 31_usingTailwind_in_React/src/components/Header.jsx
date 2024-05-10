1633b95154d07f79f93c38140b70474e9f2b20a2
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-05-10 11:43:16 +0530

    ..

--- 31_usingTailwind_in_React/src/components/Header.jsx
+++ 31_usingTailwind_in_React/src/components/Header.jsx
@@ -1,20 +1,22 @@
-import viteLogo from "vite.svg";
+import viteLogo from "../../public/vite.svg";
+import { NavLink } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 
 export default function Header() {
   return (
     <header className="flex justify-between py-4 px-4 md:px-8 shadow-md pb-4">
       <img src={viteLogo} alt="logo" />
-      <ul className="flex gap-4">
+      <ul className="flex gap-2 md:gap-4">
         <li>
-          <a className="" href="/">
-            Home
-          </a>
+          <NavLink to={Home}>Home</NavLink>
         </li>
         <li>
-          <a href="/">About</a>
+          <NavLink to={About}>About</NavLink>
         </li>
         <li>
-          <a href="/">Contact</a>
+          <NavLink to={Contact}>Contact</NavLink>
         </li>
       </ul>
     </header>
--- 31_usingTailwind_in_React/src/main.jsx
+++ 31_usingTailwind_in_React/src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
