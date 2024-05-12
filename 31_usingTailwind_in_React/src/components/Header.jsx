72fa7a776ccf04bdf52913d62f24e1156840ac1b
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-05-12 13:26:20 +0530

    src/components/Header.jsx

--- 31_usingTailwind_in_React/src/components/Header.jsx
+++ 31_usingTailwind_in_React/src/components/Header.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import viteLogo from "/vite.svg";
 import { NavLink } from "react-router-dom";
+import Model from "./Model";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="flex justify-between px-4 py-4 shadow-md md:px-8">
       <img src={viteLogo} alt="viteLogo" />
       <ul className="flex gap-4">
         <li>
           <NavLink
-            className={({ isActive }) => isActive && "text-blue-700 underline"}
+            className={({ isActive }) =>
+              isActive ? "text-blue-700 underline" : ""
+            }
             to="/"
           >
             Home
@@ -17,7 +21,9 @@ export default function Header() {
         </li>
         <li>
           <NavLink
-            className={({ isActive }) => isActive && "text-blue-700 underline"}
+            className={({ isActive }) =>
+              isActive ? "text-blue-700 underline" : ""
+            }
             to="/about"
           >
             About
@@ -25,13 +31,47 @@ export default function Header() {
         </li>
         <li>
           <NavLink
-            className={({ isActive }) => isActive && "text-blue-700 underline"}
+            className={({ isActive }) =>
+              isActive ? "text-blue-700 underline" : ""
+            }
             to="/contact"
           >
             Contact
           </NavLink>
         </li>
-        <li>Sign In</li>
+        <button onClick={() => setIsOpen(true)}>Sign In</button>
+        <Model
+          header={<div className="text-xl font-bold">Sign In</div>}
+          footer={
+            <div className="flex justify-end gap-4">
+              <button
+                onClick={() => setIsOpen(false)}
+                className="rounded-md bg-gray-300 px-6 py-2 font-semibold hover:bg-gray-400/80 active:bg-gray-400/60"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={() => setIsOpen(false)}
+                className="rounded-md bg-blue-300 px-6 py-2 font-semibold hover:bg-blue-400/80 active:bg-blue-400/60"
+              >
+                Sign In
+              </button>
+            </div>
+          }
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+        >
+          <input
+            placeholder="Username"
+            className="grow rounded border border-gray-600 px-2 py-1"
+            type="text"
+          />
+          <input
+            placeholder="Password"
+            className="grow rounded border border-gray-600 px-2 py-1"
+            type="password"
+          />
+        </Model>
       </ul>
     </header>
   );
