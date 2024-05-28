8214dc3c12a52e0fccc8ff19293c39ee5456fa73
failedCoderrr <pankajkori2057@gmail.com>
2024-05-28 20:20:45 +0530

    code execution

---
+++ JS/codes/03_code_execution/script.js
@@ -0,0 +1,18 @@
+debugger;
+console.log(firstName);
+
+var firstName = "Adarsh";
+let lastName = "Singh";
+let age = 15;
+
+const hoursInDay = 24;
+
+let userIntro =
+  "Hi, my name is " +
+  firstName +
+  " " +
+  lastName +
+  ". I am " +
+  age +
+  " " +
+  "years old.";
