c9064b6ad11990b2cb583bf694d238ff23707333
failedCoderrr <pankajkori2057@gmail.com>
2024-05-29 18:43:10 +0530

    ..

---
+++ JS/codes/05_template_literals/script.js
@@ -0,0 +1,22 @@
+// strings are indexed
+const string1 = "nothing but a string indexing";
+// PROPERTIES of the string
+console.log(string1[5]); //index value of string at index 6
+
+// checking the length of the string
+const stringLength = string1.length;
+
+// METHOD of the string
+
+// uppercase of the string
+const upperCaseString = string1.toUpperCase();
+
+// lowercase of the string
+const lowerCaseString = string1.toLocaleLowerCase();
+
+// trim method to remove extra spaces
+const spaceString = "     hello    ";
+const trimmedString = spaceString.trim();
+
+// trimStart() - to remove extra spaces from front
+const trimmedStart = spaceString.trimStart();
