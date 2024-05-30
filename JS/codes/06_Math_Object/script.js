f1e36566978c0465a0426dd9a3a5faaa137fc042
failedCoderrr <pankajkori2057@gmail.com>
2024-05-30 08:16:13 +0530

    Math Object in JS

---
+++ JS/codes/06_Math_Object/script.js
@@ -0,0 +1,15 @@
+// Math Object in JS
+
+// Remainder operator   - gives the remaining value after Divisible by Divisor
+const remainder = 13 % 3;
+
+// Exponentiation operator - power of n to its power number
+const exponent = 2 ** 5;
+
+// LN_2 - log base 2
+// LN_10 - log base 10
+// PI - Math.PI
+// Math.SQRT1_2 - 1/2 sqrt
+// Math.SQRT_2 - sqrt2
+// Math.sqrt() - sqrt of any number
+// Math.pow() - power of any number
