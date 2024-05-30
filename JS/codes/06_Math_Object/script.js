a51d4305bd3c74de79393495dc1a60e6071bdb02
failedCoderrr <pankajkori2057@gmail.com>
2024-05-30 08:35:54 +0530

    script.js

--- JS/codes/06_Math_Object/script.js
+++ JS/codes/06_Math_Object/script.js
@@ -13,3 +13,12 @@ const exponent = 2 ** 5;
 // Math.SQRT_2 - sqrt2
 // Math.sqrt() - sqrt of any number
 // Math.pow() - power of any number
+
+// Math.floor - removes the decimal value after the natural number/integer but when after decimal value reach upto 16 digit it goes up value like if the value is 4.9999999999999999 it will be 5 not 4 coz after decimal there's 16 digit so
+const floorValPos = Math.floor(4.9999999999999999);
+
+const floorValNeg = Math.floor(-2.66666); // result will be 3
+
+// Math.ceil() - add 1 to the before decimal value Eg: Math.ceil(4.07) will be 5 & Math.ceil(-5.87) will be 5
+const ceilValPos = Math.ceil(4.07);
+const ceilValNeg = Math.ceil(-5.87);
