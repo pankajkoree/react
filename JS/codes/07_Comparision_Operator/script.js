086a3a397ee9ce95b3614c02a2157a6f50c37240
failedCoderrr <pankajkori2057@gmail.com>
2024-05-30 20:26:37 +0530

    ..

---
+++ JS/codes/07_Comparision_Operator/script.js
@@ -0,0 +1,9 @@
+// -----Comparison Operator------
+
+// Equality Comparison  - it does implicit(automatically)conversion if data type isn't suitable
+const equalCompare = 22 == "22";
+
+// strict equality operator - compares datatype with value - it requires explicit conversion
+const strictEqualCompare = 22 === "22";
+
+// not equality operator
