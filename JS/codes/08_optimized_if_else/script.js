19c12902f92247582e0996bfadd3ba8584d21bd7
failedCoderrr <pankajkori2057@gmail.com>
2024-05-30 22:00:40 +0530

    script.js

--- JS/codes/08_optimized_if_else/script.js
+++ JS/codes/08_optimized_if_else/script.js
@@ -7,24 +7,16 @@ console.log(`Age: ${userAge}`);
 if (userAge >= 0 && userAge <= 4) {
   console.log(`${username} is a kid.`);
   console.log("And he/she is playing.");
-}
-
-if (userAge >= 5 && userAge <= 17) {
+} else if (userAge >= 5 && userAge <= 17) {
   console.log(`${username} is a school student.`);
   console.log("And he/she is learning science and maths.");
-}
-
-if (userAge >= 18 && userAge <= 24) {
+} else if (userAge >= 18 && userAge <= 24) {
   console.log(`${username} is a college student.`);
   console.log("And he/she is learning computer science.");
-}
-
-if (userAge >= 25 && userAge <= 45) {
+} else if (userAge >= 25 && userAge <= 45) {
   console.log(`${username} is a working professional.`);
   console.log("And he/she is a web developer.");
-}
-
-if (userAge > 45) {
+} else if (userAge > 45) {
   console.log(`${username} is retired.`);
   console.log("And he/she reads newspaper.");
 }
