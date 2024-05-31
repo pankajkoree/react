bed9b54073451089159d5d55695b0c4362ef8b8e
failedCoderrr <pankajkori2057@gmail.com>
2024-05-31 12:39:19 +0530

    script.js

--- JS/codes/11_Objects_inJS/script.js
+++ JS/codes/11_Objects_inJS/script.js
@@ -4,6 +4,21 @@ const myName = "ritik";
 const userName1 = ""; // addresses of two same strings are same but in case of objects its different
 const userName2 = "";
 
-const user = {};
+const user = {
+  fname: "pankaj",
+}; // comparing the two objects gives false, coz they compare their addresses not the values
 
-const user2 = {};
+const user2 = {
+  fname: "ravi",
+  "last-name": "bishnoi", // giving string to the key is optional but when you use kebab case, its compulsory to make the key a string
+};
+
+// accessing the value from objects
+// using the dot (.) notation
+console.log(user.fname);
+
+// we can't access the string key using the dot notation
+console.log(user2.last - name); // it throws an error or gives a NaN value
+
+// so the correct way to access them is using bracket notation
+console.log(user2["last-name"]);
