f69c45eb62f9ccbfc1bc32f40df85ad577f47bb8
failedCoderrr <pankajkori2057@gmail.com>
2024-05-31 08:33:53 +0530

    v.1.1

--- JS/codes/09_nestedIfElse_SwitchCase/script.js
+++ JS/codes/09_nestedIfElse_SwitchCase/script.js
@@ -1,7 +1,29 @@
 // Nested If else
 // Grade determination
 
-const marks = +prompt("Enter the marks: ");
+// const marks = +prompt("Enter the marks: ") || 40; // default marks
+const marks = 40; //default marks
 if (marks >= 40) {
-  console.log("Student is passed.");
+  if (marks >= 90) {
+    console.log("A+");
+  } else if (marks >= 80 && marks < 90) {
+    console.log("A");
+  } else if (marks >= 70 && marks < 80) {
+    console.log("B+");
+  } else if (marks >= 60 && marks < 70) {
+    console.log("B");
+  } else if (marks >= 50 && marks < 60) {
+    console.log("C+");
+  } else {
+    console.log("C");
+  }
+} else {
+  if (marks < 40 && marks >= 35) {
+    console.log("D grade");
+  } else {
+    console.log("Not graded - NG");
+  }
 }
+
+// Switch Case
+// Room allotment
