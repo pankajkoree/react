de2645a8b94aeb8c2dc000217d56200deab6a449
failedCoderrr <pankajkori2057@gmail.com>
2024-05-31 12:22:02 +0530

    Objects

---
+++ JS/codes/11_Objects_inJS/script.js
@@ -0,0 +1,9 @@
+// creating a objects
+
+const myName = "ritik";
+const userName1 = ""; // addresses of two same strings are same but in case of objects its different
+const userName2 = "";
+
+const user = {};
+
+const user2 = {};
