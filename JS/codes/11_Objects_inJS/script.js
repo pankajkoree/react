a9921478a39ae4324e220667d1d569678e13fa32
failedCoderrr <pankajkori2057@gmail.com>
2024-05-31 20:35:58 +0545

    script.js

--- JS/codes/11_Objects_inJS/script.js
+++ JS/codes/11_Objects_inJS/script.js
@@ -11,6 +11,7 @@ const user = {
 const user2 = {
   fname: "ravi",
   "last-name": "bishnoi", // giving string to the key is optional but when you use kebab case, its compulsory to make the key a string
+  ritik: "student",
 };
 
 // accessing the value from objects
@@ -22,3 +23,9 @@ console.log(user2.last - name); // it throws an error or gives a NaN value
 
 // so the correct way to access them is using bracket notation
 console.log(user2["last-name"]);
+
+// we can also access the js variables using the [] notation
+console.log(user2[myName]); //myName is variable with value ritik and that ritik is the key in user2 object
+
+// using []notation, expression can also be written
+console.log(user2["last" + "-" + "name"]); // this expression results in last-name which is the key in Object user2
