9abef5ff6152d3e4e999b25f96a25ceeacc2dc45
failedCoderrr <pankajkori2057@gmail.com>
2024-06-01 09:13:40 +0545

    scripts.js

--- JS/codes/12_Arrays_in_Js/scripts.js
+++ JS/codes/12_Arrays_in_Js/scripts.js
@@ -3,3 +3,12 @@ const fruitsCollection = ["Apple", "Banana", "Lemon", "Grapes", "Litchi"];
 
 // can access the element using [] bracket notation
 const accessFruits = fruitsCollection[2];
+
+// can't assign the new array to the same existing array Eg:
+// fruitsCollection = []; // gives error as TypeError: Assignment to constant variable.
+
+// adding a objects as an item in array
+fruitsCollection.push({
+  potato1: "potato",
+  potato2: "sweetpotato",
+});
