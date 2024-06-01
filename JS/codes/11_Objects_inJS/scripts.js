4039d8cbc82dc38b205ee77d2ae5bba4737745ba
failedCoderrr <pankajkori2057@gmail.com>
2024-06-01 07:19:46 +0545

    scripts.js

---
+++ JS/codes/11_Objects_inJS/scripts.js
@@ -0,0 +1,15 @@
+const user = {
+  firstName: "Adarsh",
+  lastName: "Singh",
+  location: {
+    city: "Bangalore",
+    pinCode: 876876,
+    state: "Karnataka",
+    moreDetails: {
+      population: 368763478,
+      area: "787 sq km",
+    },
+  },
+  age: 15,
+  isGraduate: false,
+};
