cafa19889418b4aafd561bc2e50ac4a61722c8a4
failedCoderrr <pankajkori2057@gmail.com>
2024-06-01 08:47:26 +0545

    Arrays

---
+++ JS/codes/12_Arrays_in_Js/scripts.js
@@ -0,0 +1,5 @@
+// declaring an arrays  - its type would be Object
+const fruitsCollection = ["Apple", "Banana", "Lemon", "Grapes", "Litchi"];
+
+// can access the element using [] bracket notation
+const accessFruits = fruitsCollection[2];
