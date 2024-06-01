66a929e4dec0e580dac22369a81388eae0ba445a
failedCoderrr <pankajkori2057@gmail.com>
2024-06-01 09:47:32 +0545

    scripts.js

--- JS/codes/12_Arrays_in_Js/scripts.js
+++ JS/codes/12_Arrays_in_Js/scripts.js
@@ -12,3 +12,40 @@ fruitsCollection.push({
   potato1: "potato",
   potato2: "sweetpotato",
 });
+
+// adding some empty items as undefined
+fruitsCollection[10] = [];
+
+// adding an item in array at last - normal way and right way
+fruitsCollection[fruitsCollection.length] = "Kiwi"; // normal way
+
+fruitsCollection.push("WaterMelon"); //right way
+
+// deleting/removing from last
+fruitsCollection.pop();
+
+//trying to add key value pairs in array
+// fruitsCollection[firstName:"Hello"]  // gives error if we add key value pairs like that in array
+
+// a new way to add key value pairs but it's not recommended to use and its length won't be increasing, it remains 0
+const newArray = [];
+newArray.firstName = "Apple";
+newArray.lastName = "Fruits";
+
+// adding object in unusual way to array
+const newObject = {
+  rollno: 40,
+  age: 44,
+};
+newObject.state = "CG";
+
+// small task - rainBow color
+const rainbowColor = [
+  "Violet",
+  "Indigo",
+  "Blue",
+  "Green",
+  "Orange",
+  "Yellow",
+  "Red",
+];
