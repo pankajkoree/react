c150e9fab643f2b7a130061e67db8a9c319fbce4
failedCoderrr <pankajkori2057@gmail.com>
2024-06-02 12:12:25 +0545

    script.js

--- JS/codes/14_DeepCopy_VS_ShallowCopy/script.js
+++ JS/codes/14_DeepCopy_VS_ShallowCopy/script.js
@@ -27,3 +27,13 @@ const user2 = user;
 user2.lname = "chaubey";
 
 user.fname = "Ajay"; // why in objects changing one makes changes in another too, coz it can be updated using . and [] notation but in case of string it's not possible which means updating in string means assigning a different value which creates another memory locations
+
+// copying the object
+const user3 = {};
+
+Object.assign(user3, user); // it is used earlier but after ES6 new way come
+user3.fname = "Lalit";
+
+// using spread operator
+const user4 = { ...user2 };
+user4.fname = "ravi";
