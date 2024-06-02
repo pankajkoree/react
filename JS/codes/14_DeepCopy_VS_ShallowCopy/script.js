66d00f2979dcb414d1e2c80166c628705265f330
failedCoderrr <pankajkori2057@gmail.com>
2024-06-02 10:29:33 +0545

    Deep copy VS shallow copy

---
+++ JS/codes/14_DeepCopy_VS_ShallowCopy/script.js
@@ -0,0 +1,24 @@
+// // creating an array
+// const fruits = ["Mango", "Apple", "Orange"];
+
+// // new array with the assignment of another array
+// const myFruits = fruits;
+
+// // pushing item into array - but shows weird behavior
+// myFruits.push("Dates");
+// myFruits.push("Grapes");
+
+// // the same behavior with objects
+// const user = {
+//   fname: "ritik",
+//   lname: "singh",
+// };
+
+// const user2 = user;
+// user2.lname = "chaubey";
+
+// these two variable will have the same memory address
+const userName1 = "pk";
+let userName2 = userName1;
+
+// changing the memory address by adding something in the variable
