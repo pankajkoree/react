788ddae0275cf4bbe1b9c6bdff7a1b9546bafd4e
failedCoderrr <pankajkori2057@gmail.com>
2024-06-02 11:39:40 +0545

    script.js

--- JS/codes/14_DeepCopy_VS_ShallowCopy/script.js
+++ JS/codes/14_DeepCopy_VS_ShallowCopy/script.js
@@ -1,24 +1,25 @@
-// // creating an array
-// const fruits = ["Mango", "Apple", "Orange"];
-
-// // new array with the assignment of another array
-// const myFruits = fruits;
-
-// // pushing item into array - but shows weird behavior
-// myFruits.push("Dates");
-// myFruits.push("Grapes");
-
-// // the same behavior with objects
-// const user = {
-//   fname: "ritik",
-//   lname: "singh",
-// };
-
-// const user2 = user;
-// user2.lname = "chaubey";
-
 // these two variable will have the same memory address
-const userName1 = "pk";
-let userName2 = userName1;
+// const userName1 = "pk";
+// let userName2 = userName1;
 
 // changing the memory address by adding something in the variable
+// userName2 = userName2 + " chaubey";
+
+// creating an array
+const fruits = ["Mango", "Apple", "Orange"];
+
+// new array with the assignment of another array
+const myFruits = fruits;
+
+// pushing item into array - but shows weird behavior
+myFruits.push("Dates");
+myFruits.push("Grapes");
+
+// the same behavior with objects
+const user = {
+  fname: "ritik",
+  lname: "singh",
+};
+
+const user2 = user;
+user2.lname = "chaubey";
