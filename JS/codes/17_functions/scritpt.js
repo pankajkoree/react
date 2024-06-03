577a94a43c359aa78df0676ee790862c050f764a
failedCoderrr <pankajkori2057@gmail.com>
2024-06-03 21:28:22 +0545

    function

---
+++ JS/codes/17_functions/scritpt.js
@@ -0,0 +1,10 @@
+// creating a function
+function introduction() {
+  console.log("hi");
+  console.log("whats the name");
+}
+
+console.log("***********");
+
+// calling a function
+introduction();
