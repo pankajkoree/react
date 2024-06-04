54f5115b79abbeb2ba0863051ce750d95da52327
failedCoderrr <pankajkori2057@gmail.com>
2024-06-04 14:26:46 +0545

    execution context

--- JS/codes/19_execution_context/execution_context.js
+++ JS/codes/19_execution_context/execution_context.js
@@ -10,12 +10,13 @@
 
 // let userIntro = "Hi, my name is " + firstName + " " + lastName;
 debugger;
+test(); //calling the function before , doesn't throw error if it's initialized
 const userName = "pankaj"; // at first this line, it creates a memory for the variable userName and initialize the value as undefined
 console.log(userName);
 
-test();
-
-// funtion execution context
+// funtion execution context - it gets initialized at the time of memory creation
 function test() {
-  console.log("Hi");
+  const a = 14; // for the function, it creates its own local execution context
+  const b = 12;
+  console.log(a, b);
 }
