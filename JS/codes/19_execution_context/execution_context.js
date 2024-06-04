754360c191e54f91629c4cc932de45c2758ead0c
failedCoderrr <pankajkori2057@gmail.com>
2024-06-04 19:00:30 +0545

    execution context and callstack

--- JS/codes/19_execution_context/execution_context.js
+++ JS/codes/19_execution_context/execution_context.js
@@ -1,32 +1,41 @@
-// execution context in Global
+// // execution context in Global
 
+// // debugger;
+// // console.log(firstName);
+
+// // var firstName = "Akash";
+// // let lastName = "Singh";
+// // let age = 23;
+// // const yearOfBirth = 2000;
+
+// // let userIntro = "Hi, my name is " + firstName + " " + lastName;
 // debugger;
-// console.log(firstName);
+// test(); //calling the function before , doesn't throw error if it's initialized
+// const userName = "pankaj"; // at first this line, it creates a memory for the variable userName and initialize the value as undefined
+// console.log(userName);
 
-// var firstName = "Akash";
-// let lastName = "Singh";
-// let age = 23;
-// const yearOfBirth = 2000;
+// // funtion execution context - it gets initialized at the time of memory creation
+// function test() {
+//   const a = 14; // for the function, it creates its own local execution context
+//   const b = 12;
+//   console.log(a, b);
+//   add(4, 5);
+// }
 
-// let userIntro = "Hi, my name is " + firstName + " " + lastName;
-// debugger;
-test(); //calling the function before , doesn't throw error if it's initialized
-const userName = "pankaj"; // at first this line, it creates a memory for the variable userName and initialize the value as undefined
-console.log(userName);
+// function add(x, y) {
+//   sub(x, y);
+//   return x + y;
+// }
 
-// funtion execution context - it gets initialized at the time of memory creation
-function test() {
-  const a = 14; // for the function, it creates its own local execution context
-  const b = 12;
-  console.log(a, b);
-  add(4, 5);
+// function sub(x, y) {
+//   return x - y;
+// }
+
+debugger;
+function introduceMe() {
+  console.log("Hi, my name is pankaj");
+  introduceMe();
 }
 
-function add(x, y) {
-  sub(x, y);
-  return x + y;
-}
-
-function sub(x, y) {
-  return x - y;
-}
+introduceMe();
+console.log("Program ended");
