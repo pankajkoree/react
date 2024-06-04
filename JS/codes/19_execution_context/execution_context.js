025897700bd4ec8d283adc12693a315414eb713a
failedCoderrr <pankajkori2057@gmail.com>
2024-06-04 14:17:05 +0545

    js file for execution_context

---
+++ JS/codes/19_execution_context/execution_context.js
@@ -0,0 +1,21 @@
+// execution context in Global
+
+// debugger;
+// console.log(firstName);
+
+// var firstName = "Akash";
+// let lastName = "Singh";
+// let age = 23;
+// const yearOfBirth = 2000;
+
+// let userIntro = "Hi, my name is " + firstName + " " + lastName;
+debugger;
+const userName = "pankaj"; // at first this line, it creates a memory for the variable userName and initialize the value as undefined
+console.log(userName);
+
+test();
+
+// funtion execution context
+function test() {
+  console.log("Hi");
+}
