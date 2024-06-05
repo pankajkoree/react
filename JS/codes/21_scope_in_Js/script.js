ebff7ea1f071357fdc8dd4c91d19893894a7968a
failedCoderrr <pankajkori2057@gmail.com>
2024-06-05 20:32:53 +0545

    script.js

--- JS/codes/21_scope_in_Js/script.js
+++ JS/codes/21_scope_in_Js/script.js
@@ -1,17 +1,30 @@
-debugger;
+// debugger;
 const userName = "Ritik"; //both let and const goes in gloabl scope under script section and can't be accessed by window.varName
 let useAge = 24;
 var aesehi = 23; // declared with var, goes in Global under window and can be accessed using window.variable_name
 
-function add() {
-  //it goes under global windows section
-  // variable declared inside the function can't be accessed outside
-  const x = 5;
-  const y = 7;
-  console.log(x + y);
+// function add() {
+//   //it goes under global windows section
+//   // variable declared inside the function can't be accessed outside
+//   const x = 5;
+//   const y = 7;
+//   console.log(x + y);
+// }
+function subtract() {
+  debugger;
+  const aa = 15;
+  const ab = 18;
+  console.log(aa - ab);
+  console.log(userName);
+
+  function child() {
+    const childName = "Chhotu";
+    console.log(childName);
+  }
 }
 
-add();
+// add();
+subtract();
 // console.log(x); // thorws the error coz of its scope
 
 console.log("program ended");
