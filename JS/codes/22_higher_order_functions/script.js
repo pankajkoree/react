a5feea583848eb9f3daca402014434d3b8c2d704
failedCoderrr <pankajkori2057@gmail.com>
2024-06-05 22:32:02 +0545

    script.js

--- JS/codes/22_higher_order_functions/script.js
+++ JS/codes/22_higher_order_functions/script.js
@@ -1,3 +1,23 @@
-function aa() {
+function aa(b) {
+  console.dir(b);
+  //   console.log(typeof b);
+
+  //   b(); // throw error as b isn't a function
+  b();
+}
+
+// aa("hii");
+// aa({ name: "pk", age: 23 });
+// aa([2, 3, 4]);
+
+//checking whether something is object or not
+// console.dir(aa); // it it remains same means it's not object else everything is object
+
+// aa.age = 24;
+// console.log(aa.age);
+
+function sayHi() {
   console.log("hii");
 }
+
+aa(sayHi);
