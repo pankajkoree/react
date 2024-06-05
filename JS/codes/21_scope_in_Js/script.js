0a9d4c6dbfa8f47c98af1edef12dfe95779b4eb4
failedCoderrr <pankajkori2057@gmail.com>
2024-06-05 08:08:53 +0545

    scope

--- JS/codes/21_scope_in_Js/script.js
+++ JS/codes/21_scope_in_Js/script.js
@@ -1,8 +1,10 @@
 debugger;
-const userName = "Ritik";
-const useAge = 24;
+const userName = "Ritik"; //both let and const goes in gloabl scope under script section and can't be accessed by window.varName
+let useAge = 24;
+var aesehi = 23; // declared with var, goes in Global under window and can be accessed using window.variable_name
 
 function add() {
+  //it goes under global windows section
   // variable declared inside the function can't be accessed outside
   const x = 5;
   const y = 7;
