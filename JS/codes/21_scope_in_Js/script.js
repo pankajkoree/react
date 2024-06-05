b74d694c25d64eca393dee0f5105b9d613e18bf0
failedCoderrr <pankajkori2057@gmail.com>
2024-06-05 21:53:49 +0545

    script.js

--- JS/codes/21_scope_in_Js/script.js
+++ JS/codes/21_scope_in_Js/script.js
@@ -14,13 +14,31 @@ function subtract() {
   debugger;
   const aa = 15;
   const ab = 18;
+  const ac = 20;
   console.log(aa - ab);
   console.log(userName);
 
   function child() {
+    // won't show it in memory execution phase if it's not being called or used
     const childName = "Chhotu";
     console.log(childName);
+    console.log(ac);
+    function grandChild() {
+      const grandChildName = "ree";
+      console.log(grandChildName);
+    }
+    // lexical scope means a function have access upto where or the area where it can be freely accessed
+    grandChild();
+    // console.log(grandChildName); throws the error when accessing the inside function items outside the functions
   }
+  // block scope {}
+  {
+    let num1 = 23;
+    console.log(num1);
+  }
+  // block scope till here
+
+  child();
 }
 
 // add();
