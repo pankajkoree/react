732182690e84f47542e6d9196e8d85eee409a627
failedCoderrr <pankajkori2057@gmail.com>
2024-06-06 12:36:17 +0545

    setTimeout and setInterval

---
+++ JS/codes/23_setTimeOut_and_setInterval/script.js
@@ -0,0 +1,4 @@
+// setTimeout isn't the part of the js, its a part of web browser but used in js
+// setTimeout("hii"); //doesn;t a expect a string, but a js code
+
+setTimeout("console.log('hi')"); // in output this output is shown but the source file won't be this, it will create its own file based on this code file
