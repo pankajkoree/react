8650269bdcc1187df260523890fb82a87daf05ab
failedCoderrr <pankajkori2057@gmail.com>
2024-06-07 09:44:24 +0545

    arrow functions

---
+++ JS/codes/27_arrow_functions/script.js
@@ -0,0 +1,26 @@
+// ------------Arrow functions------------------
+
+// normal function
+function square(num) {
+  return num ** 2;
+}
+
+// anonymous function
+const square1 = function (num) {
+  return num ** 2;
+};
+
+// arrow function for square function
+const squarefn = (num) => {
+  return num ** 2;
+};
+
+// when we have only one statement in arrow function i.e.
+const sqfn = (num) => num ** 2; // implicit return means we don't use return keyword to return
+
+// ------------**************--------------------
+
+// add arrow funtions
+const add = (num1, num2) => num1 + num2;
+
+// ------------**************--------------------
