14faeefddea2ec7b14258591c15cf9b43ef1c14a
failedCoderrr <pankajkori2057@gmail.com>
2024-06-07 19:23:10 +0545

    forEach

--- JS/codes/29_forEach/script.js
+++ JS/codes/29_forEach/script.js
@@ -21,6 +21,7 @@ const bikes = ["ducati", "fz", "BMW", "R15", "KTM", "DUKE", "BULLET"];
 //   // passing an arrow function in forEach
 //   console.log(bike);
 // });
+// passing these code directly in console will need a return value as it's result but it still throws undefined coz forEach is its own function and can't be returned anything
 
 // ------------------------------------------------
 
@@ -30,7 +31,7 @@ function fEach(el) {
   console.log(el);
 }
 
-// passing above named function in the for Each
+// passing above named function in the for Each // but this isn't a recommended method to pass a named function which is outside of the forEach
 bikes.forEach(fEach);
 
 // ------------------------------------------------
