75bd23ddb9a6359494e5deae020ba38148b3d490
failedCoderrr <pankajkori2057@gmail.com>
2024-06-07 08:54:27 +0545

    function vs methods

--- JS/codes/26_function_VS_methods/script.js
+++ JS/codes/26_function_VS_methods/script.js
@@ -1,3 +1,30 @@
 // -------functions VS methods----------------
+// Every methods are functions but every functions aren't methods
+// for being a functions a method, the function should be inside a object
 
+const mathsfn = {
+  PI: 3.12,
+  add: function (a, b) {
+    return a + b;
+  },
+  sub: function (a, b) {
+    if (a > b) {
+      return a - b;
+    }
+    return b - a;
+  },
+  pow: function (a, b) {
+    return a ** b;
+  },
+  // a new ES6 concept to define the same function
+  subtract(a, b) {
+    if (a > b) {
+      return a - b;
+    }
+    return b - a;
+  },
+  pow1(a, b) {
+    return a ** b;
+  },
+};
 // ---------------***********--------------------
