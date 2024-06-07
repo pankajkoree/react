598ca510d6c3b46e720bf57e55e0f0569fc151fb
failedCoderrr <pankajkori2057@gmail.com>
2024-06-07 08:06:19 +0545

    understanding about closure in JS

--- JS/codes/25_return_function_in_closure/script.js
+++ JS/codes/25_return_function_in_closure/script.js
@@ -1,13 +1,40 @@
 // codes
 // ---------------------------XXXXXXXXXXXX----------------
+// Named function
+// function outer() {
+//   // it will form two closure i.e. one for outer function variable and one for parent function variable
+//   const a = 5;
+//   function parent() {
+//     const b = 3; //normally this variable should have been deleted once its exeuction ends but as we are using it in inside function and returning that function it forms closure, so it remains somewhere in memory
 
-const a = 5;
-const b = 3;
+//     //higher order functions means either it returns a function or take function as argumnets
+//     function add() {
+//       console.log(a + b);
+//     }
+//     return add;
+//   }
+//   return parent();
+// }
 
-function add() {
-  console.log(a + b);
+// const add1 = outer();
+
+// console.dir(add1);
+
+// ---------------------------XXXXXXXXXXXX----------------
+
+// anonymous function // everything same as upper code except the function add will be converted here into anonymous function
+function outer() {
+  const a = 5;
+  function parent() {
+    const b = 3;
+
+    return function () {
+      console.log(a + b);
+    };
+  }
+  return parent();
 }
 
-add();
+const add1 = outer();
 
-console.dir(add);
+console.dir(add1);
