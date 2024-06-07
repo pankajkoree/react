3b898b793e318690d321ed131be257a028410cca
failedCoderrr <pankajkori2057@gmail.com>
2024-06-07 10:21:02 +0545

    ..

--- JS/codes/27_arrow_functions/script.js
+++ JS/codes/27_arrow_functions/script.js
@@ -12,6 +12,7 @@ const square1 = function (num) {
 
 // arrow function for square function
 const squarefn = (num) => {
+  // if we use only one parameter, it's not necessary to use bracket, but its better to use
   return num ** 2;
 };
 
@@ -23,4 +24,25 @@ const sqfn = (num) => num ** 2; // implicit return means we don't use return key
 // add arrow funtions
 const add = (num1, num2) => num1 + num2;
 
+// ------------**************--------------------
+
+// using anonymous arrow functions in setTimeout
+
+setTimeout(() => {
+  console.log("hii");
+}, 1000);
+
+// ------------**************--------------------
+
+// generating a randow value from 1 to 10 using arrow functions
+const randomVal = Math.floor(Math.random() * 10) + 1;
+
+// from a specific range i.e. 20 to 30
+
+const spRandomVal = () => {
+  const min = 20;
+
+  const randomVal1 = Math.floor(Math.random() * 10) + 1 + min;
+  return randomVal1;
+};
 // ------------**************--------------------
