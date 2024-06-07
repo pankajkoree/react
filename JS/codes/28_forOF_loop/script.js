7ff5104ac78bb5e2b16146e51ead51d5bcc0160a
failedCoderrr <pankajkori2057@gmail.com>
2024-06-07 11:04:46 +0545

    for of and for in loop

--- JS/codes/28_forOF_loop/script.js
+++ JS/codes/28_forOF_loop/script.js
@@ -1,4 +1,5 @@
 // ---------- for of loop -------------------
+// it can be used in array, node list, string but can't be used in object
 
 const bikes = ["ducati", "fz", "bmw", "bullet"];
 
@@ -16,9 +17,49 @@ for (bike of bikes) {
 // --------------------------------------------------
 
 // using const in place of nothing or var
-for (const biken of bikes) {
-  debugger;
-  console.log(biken);
-} // its items will be limit to its block which means after the execution biken won't be accessible but before execution its value will remain same
+// for (const biken of bikes) {
+//   console.log(biken);
+// } // its items will be limit to its block which means after the execution biken won't be accessible but before execution its value will remain same
 
+// --------------------------------------------------
+
+// using for of in Object
+
+// const details = {
+//   name: "pk",
+//   age: 23,
+//   course: "BTCS",
+//   nation: "nepal",
+//   state: "CG",
+// };
+
+// for (const elem of details) {
+//   console.log(elem);
+// } // will throws the error as Uncaught TypeError: details is not iterable
+
+//  so whats the way to access the element of object
+// i.e. for in loop
+// --------------*********-----------------
+// ---------- for in loop -------------------
+
+const details = {
+  name: "pk",
+  age: 23,
+  course: "BTCS",
+  nation: "nepal",
+  state: "CG",
+};
+
+// for (const elem in details) {
+//   console.log(`${elem} : ${details[elem]}`); //only elem will fetch the key of object and using bracket notation we can access the value too
+// }    // but it's not a recommended way coz it takes more time to run or performance lags
+// --------------------------------------------------
+// --------------------------------------------------
+// so another way :
+const keys = Object.keys(details); //directly access the keys
+const values = Object.values(details); //directly access the values and entries will access both keys and values
+for (const key of keys) {
+  console.log(`${key} : ${details[key]}`);
+} // although it is one more line but it's performance is better
+// --------------------------------------------------
 // --------------*********-----------------
