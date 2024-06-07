72e2841d80051b8cc2671a238cc9a6f65ea9c728
failedCoderrr <pankajkori2057@gmail.com>
2024-06-07 19:42:36 +0545

    map filter reduce

---
+++ JS/codes/30_map_filter_reduce/script.js
@@ -0,0 +1,26 @@
+// ----------- MAP FILTER REDUCE----------------
+// -----------------****************---------------------
+
+// ---------------MAP----------------------------
+// map returns an array
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+// forEach doesn't return anything
+months.forEach(function (month) {
+  console.log(month);
+});
+
+// -----------------****************---------------------
