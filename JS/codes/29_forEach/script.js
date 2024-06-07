a2c2aa610e15205ea245d8ba206641f949f0bf3b
failedCoderrr <pankajkori2057@gmail.com>
2024-06-07 19:15:18 +0545

    forEach

--- JS/codes/29_forEach/script.js
+++ JS/codes/29_forEach/script.js
@@ -1,20 +1,37 @@
 // --------------for Each -----------------------
 
 const bikes = ["ducati", "fz", "BMW", "R15", "KTM", "DUKE", "BULLET"];
-
+// ------------------------------------------------
 // for of loops
 // for (const bike of bikes) {
 //   console.log(bike);
 // }
+// ------------------------------------------------
 
+// ------------------------------------------------
 // using the for each
 // bikes.forEach(function (bike) {  //passing anonymous functions in forEach
 //   console.log(bike);
 // });
+// ------------------------------------------------
 
+// ------------------------------------------------
 // for Each is a higher order functions
-bikes.forEach((bike) => {
-  // passing an arrow function in forEach
-  console.log(bike);
-});
+// bikes.forEach((bike) => {
+//   // passing an arrow function in forEach
+//   console.log(bike);
+// });
+
+// ------------------------------------------------
+
+// ------------------------------------------------
+// named function
+function fEach(el) {
+  console.log(el);
+}
+
+// passing above named function in the for Each
+bikes.forEach(fEach);
+
+// ------------------------------------------------
 // ---------------***********--------------------
