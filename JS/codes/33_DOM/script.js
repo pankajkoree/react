e64b72e0e8c81c9d4dea054732b0a2e5418409cd
failedCoderrr <pankajkori2057@gmail.com>
2024-06-08 22:09:18 +0545

    DOM

--- JS/codes/33_DOM/script.js
+++ JS/codes/33_DOM/script.js
@@ -4,4 +4,7 @@ function sayHi() {
   console.log("hi");
 }
 
+// -----------------------------------------------------
+// using innerHTML, we can add tags too like --- h1.innerHTML = "<i>PK</i>"
+//but using, innerText, we can't add those tags, adding tags doesn't affect but seems as same as in HTML too
 // ------------**************************------------------
