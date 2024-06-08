35c589dc76941efc74d0be6ec493e661c3327d6e
failedCoderrr <pankajkori2057@gmail.com>
2024-06-08 10:18:32 +0545

    some and every

--- JS/codes/31_some_and_every/script.js
+++ JS/codes/31_some_and_every/script.js
@@ -1,12 +1,21 @@
 // ----------some and every array methods-------------
 
-const evenNumbers = [0, 2, 4, 6, 8, 10];
+const evenNumbers = [0, 2, 3, 4, 6, 8, 10];
 
+// -----------------------------------------------------
 // if any of the value from the times of array, results in true, the whole result will be true
 // once it founds a value to be true, it stops its execution, means further it don't do further processing
-const someResult = evenNumbers.some((num, i, array) => {
-  debugger;
-  return num > 4;
+// const someResult = evenNumbers.some((num, i, array) => {
+//   return num > 4;
+// });
+// -----------------------------------------------------
+
+// checking whether the array is perfect even or not
+
+const isEven = evenNumbers.some((num) => {
+  return num % 2 == 1;
 });
+
+// -----------------------------------------------------
 
 // --------------***************--------------------
