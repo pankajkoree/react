a350cfa9f6d6e109c1d103f37c8441ca747de5b7
failedCoderrr <pankajkori2057@gmail.com>
2024-06-08 11:29:52 +0545

    BOM

---
+++ JS/codes/32_BOM/script.js
@@ -0,0 +1,21 @@
+// --------------BOM - browser object model -----------------
+
+// ---------------location---------------------------------
+
+// can use either window.location or location only
+// console.log(location); // gives output as
+// Location {ancestorOrigins: DOMStringList, href: 'http://127.0.0.1:5500/BOM.html', origin: 'http://127.0.0.1:5500', protocol: 'http:', host: '127.0.0.1:5500', …}
+
+// ---------------------------------------------------------
+// using location.reload() -  will reload the browser
+// console.log(location.reload());
+
+// ---------------------------------------------------------
+// using the location.href = 'url' - will change the href location
+// location.href = "www.google.com"; //as it is not present in the localhost so it won't direct to some page
+
+// location.href =
+//   "https://youtube.com/watch?v=hyIBB48aAws&list=PLfEr2kn3s-bo4LwlbyZugHPavhcdW8YMC&index=53";    // this will direct the browser to that youtube link
+
+// ---------------------------------------------------------
+// ----------------------*************----------------------
