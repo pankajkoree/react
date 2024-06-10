047b30888ea8c8ee988c1a75f20dd4b3c8077b2d
failedCoderrr <pankajkori2057@gmail.com>
2024-06-10 09:14:42 +0545

    Element VS node

---
+++ JS/codes/34_element_VS_node/script.js
@@ -0,0 +1,8 @@
+// -------------Element VS Node------------------------
+
+// ----------------------Element---------------------
+// are the tags that resides in that particular element i.e.
+
+document.body.children; //gives the result as below
+// HTMLCollection(2) [h1, script]   // gives a result of htmlCollection which is basically the elements
+// -------------------******************-------------------
