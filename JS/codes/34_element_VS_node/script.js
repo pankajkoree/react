7aee82c0bbb3200da5868b07332f142f74c2c0ce
failedCoderrr <pankajkori2057@gmail.com>
2024-06-10 09:42:00 +0545

    element vs node

--- JS/codes/34_element_VS_node/script.js
+++ JS/codes/34_element_VS_node/script.js
@@ -5,4 +5,46 @@
 
 document.body.children; //gives the result as below
 // HTMLCollection(2) [h1, script]   // gives a result of htmlCollection which is basically the elements
+
+// --------------------------------------------------
+
+// ----------------------Node------------------------
+
+document.body.childNodes;
+
+// typeof document.body.childNodes;
+// ("object");
+
+// the result will be and the type of the nodes will be Object
+// NodeList(7) [text, h1, text, comment, text, script, text]
+// 0
+// :
+// text
+// 1
+// :
+// h1
+// 2
+// :
+// text
+// 3
+// :
+// comment
+// 4
+// :
+// text
+// 5
+// :
+// script
+// 6
+// :
+// text
+// length
+// :
+// 7
+// [[Prototype]]
+// :
+// NodeList
+
+// --------------------------------------------------
+
 // -------------------******************-------------------
