59ffb67996a2abe410f41c6e9bae18d326b464c7
failedCoderrr <pankajkori2057@gmail.com>
2024-06-10 22:25:43 +0545

    create element

--- JS/codes/36_create_element/script.js
+++ JS/codes/36_create_element/script.js
@@ -1,6 +1,5 @@
 const h1 = document.querySelector("h1");
 const container = document.querySelector(".container");
-const card = document.querySelector(".card");
 
 // container.appendChild(h1)
 // container.appendChild(h1.cloneNode(true))
@@ -10,3 +9,16 @@ const card = document.querySelector(".card");
 //     newCard.innerText = i
 //     container.append(newCard)
 // }
+
+// --------------fetching 100 pokemon character-----------
+
+const img = document.querySelector("img");
+for (let i = 2; i <= 100; i++) {
+  const newImage = img.cloneNode();
+  newImage.src =
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+    i +
+    ".png";
+
+  container.appendChild(newImage);
+}
