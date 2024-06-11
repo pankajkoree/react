51a274c6e6cae6e382b28b04b89010dfc6442ae4
failedCoderrr <pankajkori2057@gmail.com>
2024-06-11 07:03:33 +0545

    createElement

--- JS/codes/36_create_element/script.js
+++ JS/codes/36_create_element/script.js
@@ -12,13 +12,51 @@ const container = document.querySelector(".container");
 
 // --------------fetching 100 pokemon character-----------
 
-const img = document.querySelector("img");
-for (let i = 2; i <= 100; i++) {
-  const newImage = img.cloneNode();
-  newImage.src =
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
-    i +
-    ".png";
+// const img = document.querySelector("img");
+// for (let i = 2; i <= 100; i++) {
+//   const newImage = img.cloneNode();
+//   newImage.src =
+//     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+//     i +
+//     ".png";
 
-  container.appendChild(newImage);
+//   container.appendChild(newImage);
+// }
+
+// -------------------------------------------------------
+
+// the element which will be created, that doesn't exist by default in html, will  be an inline element
+// EG:  <hi>pk</hi> // this will be inline elements as: when we see the things in computed tab for this item the resutl will be
+// display inline
+// height auto
+// width auto
+
+//---------------------------------------------------------
+
+// creating a tag and displaying it in file
+
+const paragraph = document.createElement("p");
+paragraph.innerText = "hello";
+paragraph.classList.add("paragraphElement");
+paragraph.id = "para";
+container.append(paragraph);
+
+// paragraph.innerText = "hello"; //this will instantly gets reflected coz DOM already have loaded that element in memory  //but its not a good practice to add innertext after appending  //so will do before appending
+
+//---------------------------------------------------------
+
+// creating an img tag and giving src attribute
+
+const img = document.createElement("img");
+img.src =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png";
+container.append(img);
+
+// making the above image 100 times
+for (let i = 2; i <= 100; i++) {
+  const newImg = img.cloneNode();
+  newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+  container.appendChild(newImg);
 }
+
+// ----------------*****************------------------------
