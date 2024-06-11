50898174226b351d11a9bf5f0612a9803b5d06d3
failedCoderrr <pankajkori2057@gmail.com>
2024-06-11 19:31:19 +0545

    addEventListener

--- JS/codes/38_addEventListener/script.js
+++ JS/codes/38_addEventListener/script.js
@@ -1,11 +1,32 @@
 // ---------------addEventListener-----------------------
 
+// -------------------------------------------------------
+// *******************************************************
+// this way of doing has some drawbacks, as we have to write some js code in html file which is a headache
+// -------------------------------------------------------
+
 // normally this is the way how a Angular developer uses event
-function sayHi() {
-  console.log("hiii");
-}
+// function sayHi() {
+//   console.log("hiii");
+// }
 
 // -------------------------------------------------------
+// -------------------------------------------------------
+
+// adding onclick event on the container
+// function container() {
+//   console.log("container clicked");
+// }
+
+// -------------------------------------------------------
+
+// adding function for ondoubleclick
+// function container() {
+//   console.log("double clicked on container");
+// }
+
+// -------------------------------------------------------
+// *******************************************************
 // -------------------------------------------------------
 
 // -------------------*****************--------------------
