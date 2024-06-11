31d874ba97c15950b217bb82281f548375adcff4
failedCoderrr <pankajkori2057@gmail.com>
2024-06-11 19:51:38 +0545

    addEventListener

--- JS/codes/38_addEventListener/script.js
+++ JS/codes/38_addEventListener/script.js
@@ -29,4 +29,44 @@
 // *******************************************************
 // -------------------------------------------------------
 
+// second way
+
+// we can add only a single function for the click event, although if we pass two function then it overrites the 1st and uses the latest function only
+// const h1 = document.querySelector("h1");
+// h1.onclick = sayHi;
+
+// function sayHi() {
+//   console.log("using the second way to add click event");
+// }
+
+// -------------------------------------------------------
+// *******************************************************
+// -------------------------------------------------------
+
+// 3rd way - little bit long way but best way
+// allows to add multiple events for a sinlge element
+const h1 = document.querySelector("h1");
+h1.addEventListener("click", sayHi);
+h1.addEventListener("click", sayHi2);
+
+function sayHi() {
+  console.log("hi");
+}
+
+function sayHi2() {
+  console.log("hiiiiiiiiiiii");
+}
+
+// --------------------------Task----------------------
+//adding event on card using addEventListener
+const card = document.querySelector(".card");
+
+card.addEventListener("click", () => {
+  console.log("hello for the card click");
+});
+
+// -------------------------------------------------------
+// *******************************************************
+// -------------------------------------------------------
+
 // -------------------*****************--------------------
