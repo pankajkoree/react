81a2c1f8c528d8dbf2228363a0dc61ff58bbdb91
failedCoderrr <pankajkori2057@gmail.com>
2024-06-12 21:00:52 +0545

    mouse events

--- JS/codes/41_mouse_events/script.js
+++ JS/codes/41_mouse_events/script.js
@@ -80,11 +80,58 @@ let count = 0;
 
 // ------------------mouse over---------------------------
 
-addCardButton.addEventListener("mouseover", (e) => {
+// addCardButton.addEventListener("mouseover", (e) => {
+//   console.log(e);
+//   const newCard = document.createElement("div");
+//   newCard.classList.add("addCardButton");
+//   newCard.innerText = count++;
+//   container.append(newCard);
+// });
+
+// --------------------------------------------------------
+// --------------------------------------------------------
+
+// ---------------- understanding some more mouse events where mouse word isn't attached---------------------------
+
+// ----------------wheel event--------------------------
+// wheel events gets triggered on any element while wheelling
+// addCardButton.addEventListener("wheel", (e) => {
+//   console.log(e);
+//   const newCard = document.createElement("div");
+//   newCard.classList.add("card");
+//   newCard.innerText = count++;
+//   container.append(newCard);
+// });
+
+// ----------------scroll event--------------------------
+
+// scroll event gets triggered when we scroll the page or the item, generally it scolls the whole document
+// document.addEventListener("scroll", (e) => {
+//   console.log(e);
+//   const newCard = document.createElement("div");
+//   newCard.classList.add("card");
+//   newCard.innerText = count++;
+//   container.append(newCard);
+// });
+
+// ----------------touch start-------------------------
+// get triggered whenever we touch the screen on mobile devices
+// addCardButton.addEventListener("touchstart", (e) => {
+//   console.log(e);
+//   const newCard = document.createElement("div");
+//   newCard.classList.add("card");
+//   newCard.innerText = count++;
+//   container.append(newCard);
+// });
+
+// ----------------touch end-------------------------
+// touch end means when release the touch it gets triggered
+addCardButton.addEventListener("touchend", (e) => {
   console.log(e);
   const newCard = document.createElement("div");
-  newCard.classList.add("addCardButton");
+  newCard.classList.add("card");
   newCard.innerText = count++;
   container.append(newCard);
 });
+
 // -------------------********************-----------------
