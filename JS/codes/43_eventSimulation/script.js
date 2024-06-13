7dfa41beb03691787aa7eda79e1394e25da4ac0e
failedCoderrr <pankajkori2057@gmail.com>
2024-06-13 06:56:43 +0545

    eventSimulation

--- JS/codes/43_eventSimulation/script.js
+++ JS/codes/43_eventSimulation/script.js
@@ -2,6 +2,7 @@
 
 const addCardBtn = document.querySelector(".card");
 const container = document.querySelector(".container");
+const input = document.querySelector("input");
 
 let count = 1;
 
@@ -12,18 +13,31 @@ addCardBtn.addEventListener("click", () => {
   container.append(newCard);
 });
 
-// -------------calling the addCardButton for 1000times------
-// this will load the card for 10 card in a second
+// ----------------------------------------------------------
+// -----------calling the addCardButton for 1000times--------
+// this will load the card for 20 card in a second and gets stopped when card count will be 1000 and this stopping phenomenon is done by clearInterval()
 
-const intervalId = setInterval(() => {
-  if (count > 999) {
-    clearInterval(intervalId);
-  }
-  addCardBtn.click();
-}, 50);
+// const intervalId = setInterval(() => {
+//   if (count > 999) {
+//     clearInterval(intervalId);
+//   }
+//   addCardBtn.click();
+// }, 50);
 
-// clearing the interval
+// ----------------------------------------------------------
+// -------------------input focus and blur-------------------
 
-// clearInterval(intervalId);
+// setTimeout(() => {
+//   input.focus();
+//   console.log("focused on input");
+// }, 2000);
+
+// setTimeout(() => {
+//   input.blur();
+//   console.log("input is blurred");
+// }, 4000);
+
+// ----------------------------------------------------------
+// -------------------submit form event----------------------
 
 //---------------------**************------------------------
