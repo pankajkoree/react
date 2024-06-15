9697ccadf519447a36b6cee4ec3f54c876d78697
failedCoderrr <pankajkori2057@gmail.com>
2024-06-15 09:45:29 +0545

    Focus on Today - Project #1

---
+++ JS/codes/45_focusOnToday/script.js
@@ -0,0 +1,24 @@
+// selecting the checkboxes
+const checkBoxList = document.querySelectorAll(".custom-checkbox");
+const inputFields = document.querySelectorAll(".goal-input");
+const errorLabel = document.querySelector(".error-label");
+const progressBar = document.querySelector(".progress-bar");
+
+checkBoxList.forEach((checkbox) => {
+  checkbox.addEventListener("click", (e) => {
+    const allGoalsAdded = [...inputFields].every((input) => {
+      return input.value;
+    });
+    if (allGoalsAdded) {
+      checkbox.parentElement.classList.toggle("completed");
+    } else {
+      //   errorLabel.style.display = "block"; // but its better not to add an inline css in the html using js
+      progressBar.classList.add("show-error");
+    }
+  });
+});
+inputFields.forEach((input) => {
+  input.addEventListener("focus", () => {
+    progressBar.classList.remove("show-error");
+  });
+});
