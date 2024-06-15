8f297ee5ac269a1cea1c0fa59d5dfd8a1d1ae7d6
failedCoderrr <pankajkori2057@gmail.com>
2024-06-15 22:14:05 +0545

    localStorage

--- JS/codes/46_localStorage/script.js
+++ JS/codes/46_localStorage/script.js
@@ -8,14 +8,16 @@
 // -------------------------------------------------------
 
 const nameTag = document.querySelector(".name-tag");
+const age = document.querySelector(".age-tag");
+const ageInput = document.querySelector(".ageInput");
+const nameInput = document.querySelector(".input-value");
 
 // adding the username directly from the localstorage and seeing changes in dom
-nameTag.innerText += localStorage.userName;
+// nameTag.innerText += localStorage.userName;
 
 // -------------------------------------------------------
 
 // setting the value from input in the localstorage
-const inputValue = document.querySelector(".input-value");
 
 //it sets the value at the time of input
 // inputValue.addEventListener("input", (e) => {
@@ -26,12 +28,13 @@ const inputValue = document.querySelector(".input-value");
 
 // -------------------------------------------------------
 
-nameTag.innerText = localStorage.getItem("userName");
+// alternate way of above thing, getting name from input and using localstorage updating the dom
+// nameTag.innerText = localStorage.getItem("userName");
 
-inputValue.addEventListener("input", (e) => {
-  localStorage.setItem("userName", e.target.value);
-  nameTag.innerText = localStorage.userName;
-});
+// inputValue.addEventListener("input", (e) => {
+//   localStorage.setItem("userName", e.target.value);
+//   nameTag.innerText = localStorage.userName;
+// });
 
 // -------------------------------------------------------
 
@@ -40,5 +43,50 @@ inputValue.addEventListener("input", (e) => {
 console.log(localStorage.getItem("age")); //null
 console.log(localStorage.age); //undefined
 // this is the minor difference between these two
+
+// -------------------------------------------------------
+
+// updating the age dom from getting input and localstorage variable
+
+// ageInput.addEventListener("input", (e) => {
+//   localStorage.setItem("myAge", e.target.value);
+//   age.innerText = localStorage.myAge;
+// });
+// age.innerText = localStorage.getItem("myAge");
+
+// ---------------********************--------------------
+// -------------------------------------------------------
+
+// making the object and then accessing it to update the DOM
+// validating if data available in localstorage then fetching from  it or simply adding
+const myData = JSON.parse(localStorage.getItem("myData")) || {};
+
+if (myData.name) {
+  nameTag.innerText = myData.name;
+}
+if (myData.age) {
+  age.innerText = myData.age;
+}
+
+nameInput.addEventListener("input", (e) => {
+  myData.name = e.target.value;
+
+  //   localStorage.setItem("myData", myData); // this store the value as [object,object] so to show as object we do as below:
+  localStorage.setItem("myData", JSON.stringify(myData));
+  localStorage.setItem("nameTag", myData.name);
+});
+
+ageInput.addEventListener("input", (e) => {
+  myData.age = e.target.value;
+
+  localStorage.setItem("myData", JSON.stringify(myData));
+  localStorage.setItem("age", myData.age);
+});
+
+// -------------------------------------------------------
+
+// localStorage.clear(); // deletes all the data available in localstorage
+
+// localStorage.removeItem('age')   //removes a single element
 
 // ---------------********************--------------------
