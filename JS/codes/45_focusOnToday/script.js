ab492a3c44357bc817e77d9158b7dd4f0fa45960
failedCoderrr <pankajkori2057@gmail.com>
2024-06-15 10:24:06 +0545

    Focus on Today - Project #1

--- JS/codes/45_focusOnToday/script.js
+++ JS/codes/45_focusOnToday/script.js
@@ -3,6 +3,7 @@ const checkBoxList = document.querySelectorAll(".custom-checkbox");
 const inputFields = document.querySelectorAll(".goal-input");
 const errorLabel = document.querySelector(".error-label");
 const progressBar = document.querySelector(".progress-bar");
+const progressValue = document.querySelector(".progress-value");
 
 checkBoxList.forEach((checkbox) => {
   checkbox.addEventListener("click", (e) => {
@@ -11,6 +12,7 @@ checkBoxList.forEach((checkbox) => {
     });
     if (allGoalsAdded) {
       checkbox.parentElement.classList.toggle("completed");
+      progressValue.style.width = "33.33%";
     } else {
       //   errorLabel.style.display = "block"; // but its better not to add an inline css in the html using js
       progressBar.classList.add("show-error");
