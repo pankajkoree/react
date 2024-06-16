554fb4855570e0c1a68b4205296c0427a6a72862
failedCoderrr <pankajkori2057@gmail.com>
2024-06-16 09:45:34 +0545

    Focus on Today - part 2 - Project #1

--- JS/codes/46_focusOnToday- part 2/script.js
+++ JS/codes/46_focusOnToday- part 2/script.js
@@ -4,8 +4,21 @@ const inputFields = document.querySelectorAll(".goal-input");
 const errorLabel = document.querySelector(".error-label");
 const progressBar = document.querySelector(".progress-bar");
 const progressValue = document.querySelector(".progress-value");
+const progressLabel = document.querySelector(".progress-label");
+
+const allQuotes = [
+  "Raise the bar by completing your goals!",
+  "Well begun is half done!",
+  "Just a step away, keep going!",
+  "Whoa! You just completed all the goals, time for chill :D",
+];
 
 const allGoals = JSON.parse(localStorage.getItem("allGoals")) || {};
+let completedGoalsCount = Object.values(allGoals).filter(
+  (goal) => goal.completed
+).length;
+progressValue.style.width += `${(completedGoalsCount / 3) * 100}%`;
+progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 completed`;
 
 checkBoxList.forEach((checkbox) => {
   checkbox.addEventListener("click", (e) => {
@@ -14,9 +27,32 @@ checkBoxList.forEach((checkbox) => {
     });
     if (allGoalsAdded) {
       checkbox.parentElement.classList.toggle("completed");
-      progressValue.style.width = "33.33%";
+      // progressValue.style.width = "33.33%";
       const inputId = checkbox.nextElementSibling.id;
       allGoals[inputId].completed = !allGoals[inputId].completed;
+      completedGoalsCount = Object.values(allGoals).filter(
+        (goal) => goal.completed
+      ).length;
+      progressValue.style.width = `${(completedGoalsCount / 3) * 100}%`;
+      progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 completed`;
+      // ------------------------------------------------
+      // // "own method"
+      // if (completedGoalsCount == 1) {
+      //   progressLabel.innerText = "Well begun is half done!";
+      // } else if (completedGoalsCount == 2) {
+      //   progressLabel.innerText = "Just a step away, keep going!";
+      // } else if (completedGoalsCount == 3) {
+      //   progressLabel.innerText =
+      //     "Whoa! You just completed all the goals, time for chill :D";
+      // }
+
+      // i can also do it using switch case
+
+      // ------------------------------------------------
+      // instructor method
+      progressLabel.innerText = allQuotes[completedGoalsCount];
+      // i think its the good one
+      // ------------------------------------------------
       localStorage.setItem("allGoals", JSON.stringify(allGoals));
     } else {
       //   errorLabel.style.display = "block"; // but its better not to add an inline css in the html using js
@@ -34,6 +70,10 @@ inputFields.forEach((input) => {
   });
 
   input.addEventListener("input", (e) => {
+    if (allGoals[input.id].completed) {
+      input.value = allGoals[input.id].name;
+      return;
+    }
     allGoals[input.id] = {
       name: input.value,
       completed: false,
