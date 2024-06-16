3e6360a70dd22b5467f6c7ee6eea5f9550d1bed2
failedCoderrr <pankajkori2057@gmail.com>
2024-06-16 07:37:32 +0545

    Focus on Today - part 2 - Project #1

--- JS/codes/46_focusOnToday- part 2/script.js
+++ JS/codes/46_focusOnToday- part 2/script.js
@@ -5,6 +5,8 @@ const errorLabel = document.querySelector(".error-label");
 const progressBar = document.querySelector(".progress-bar");
 const progressValue = document.querySelector(".progress-value");
 
+const allGoals = JSON.parse(localStorage.getItem("allGoals")) || {};
+
 checkBoxList.forEach((checkbox) => {
   checkbox.addEventListener("click", (e) => {
     const allGoalsAdded = [...inputFields].every((input) => {
@@ -13,6 +15,9 @@ checkBoxList.forEach((checkbox) => {
     if (allGoalsAdded) {
       checkbox.parentElement.classList.toggle("completed");
       progressValue.style.width = "33.33%";
+      const inputId = checkbox.nextElementSibling.id;
+      allGoals[inputId].completed = !allGoals[inputId].completed;
+      localStorage.setItem("allGoals", JSON.stringify(allGoals));
     } else {
       //   errorLabel.style.display = "block"; // but its better not to add an inline css in the html using js
       progressBar.classList.add("show-error");
@@ -20,7 +25,19 @@ checkBoxList.forEach((checkbox) => {
   });
 });
 inputFields.forEach((input) => {
+  input.value = allGoals[input.id].name;
+  if (allGoals[input.id].completed) {
+    input.parentElement.classList.add("completed");
+  }
   input.addEventListener("focus", () => {
     progressBar.classList.remove("show-error");
+  });
+
+  input.addEventListener("input", (e) => {
+    allGoals[input.id] = {
+      name: input.value,
+      completed: false,
+    }; //or e.target.value
+    localStorage.setItem("allGoals", JSON.stringify(allGoals));
   });
 });
