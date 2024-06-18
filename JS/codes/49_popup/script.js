cdfef46347a2d3931fad66f88e1ee581d0d34eb6
failedCoderrr <pankajkori2057@gmail.com>
2024-06-18 09:01:49 +0545

    popup

--- JS/codes/49_popup/script.js
+++ JS/codes/49_popup/script.js
@@ -1,11 +1,16 @@
-const popup = document.querySelector(".popup");
+const popup = document.querySelector(".popup-container");
 const button = document.querySelector("button");
 const closeIcon = document.querySelector(".close-icon");
+const overlay = document.querySelector(".overlay");
 
 button.addEventListener("click", () => {
-  popup.classList.add("open");
+  popup.classList.add("popup-open");
 });
 
 closeIcon.addEventListener("click", () => {
-  popup.classList.remove("open");
+  popup.classList.remove("popup-open");
+});
+
+overlay.addEventListener("click", () => {
+  popup.classList.remove("popup-open");
 });
