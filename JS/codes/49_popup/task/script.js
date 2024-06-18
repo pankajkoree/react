0c757421f3ceb2581b0a6e15dcb8e0545c62a136
failedCoderrr <pankajkori2057@gmail.com>
2024-06-18 21:10:53 +0545

    task

--- JS/codes/49_popup/task/script.js
+++ JS/codes/49_popup/task/script.js
@@ -1,11 +1,22 @@
 const subMainContent = document.querySelector(".sub-main-content");
 const button = document.querySelector(".newsletter");
+const popupMainContent = document.querySelector(".popup-main-content");
 
 // subMainContent.addEventListener("click", () => {
 //   console.log("clicked");
 // });
 
+// button.addEventListener("click", () => {
+//   button.classList.add("active");
+//   console.log("clicked");
+// });
+
 button.addEventListener("click", () => {
-  button.classList.add("active");
-  console.log("clicked");
+  popupMainContent.classList.add("open");
+  subMainContent.classList.add("open");
+});
+
+popupMainContent.addEventListener("click", () => {
+  popupMainContent.classList.add("close");
+  subMainContent.classList.add("open");
 });
