fa7152758e024dfc6a02005ef9ffebbbaa551e04
failedCoderrr <pankajkori2057@gmail.com>
2024-06-19 07:44:44 +0545

    task

--- JS/codes/49_popup/task/script.js
+++ JS/codes/49_popup/task/script.js
@@ -1,7 +1,10 @@
 const subMainContent = document.querySelector(".sub-main-content");
 const button = document.querySelector(".newsletter");
 const popupMainContent = document.querySelector(".popup-main-content");
-
+const closeIcon = document.querySelector(
+  "body > main > div > div.popup-main-content > div.close-icon > span"
+);
+const subscribeButton = document.querySelector(".button-div");
 // subMainContent.addEventListener("click", () => {
 //   console.log("clicked");
 // });
@@ -11,14 +14,26 @@ const popupMainContent = document.querySelector(".popup-main-content");
 //   console.log("clicked");
 // });
 
-// button.addEventListener("click", () => {
-//   popupMainContent.classList.add("open");
-//   subMainContent.classList.remove("open");
-//   subMainContent.classList.add("close");
-// });
+button.addEventListener("click", () => {
+  popupMainContent.classList.add("open");
+  subMainContent.classList.remove("open");
+  subMainContent.classList.add("close");
+});
 
 // popupMainContent.addEventListener("click", () => {
 //   subMainContent.classList.add("open");
 //   popupMainContent.classList.add("close");
 //   popupMainContent.classList.remove("open");
 // });
+
+closeIcon.addEventListener("click", () => {
+  subMainContent.classList.add("open");
+  popupMainContent.classList.add("close");
+  popupMainContent.classList.remove("open");
+});
+
+subscribeButton.addEventListener("click", () => {
+  subMainContent.classList.add("open");
+  popupMainContent.classList.add("close");
+  popupMainContent.classList.remove("open");
+});
