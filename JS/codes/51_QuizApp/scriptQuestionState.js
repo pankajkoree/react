ef4ec3a23b29d9db73e70ebd7a311591bcda7e32
failedCoderrr <pankajkori2057@gmail.com>
2024-06-19 22:12:16 +0545

    Quiz App

---
+++ JS/codes/51_QuizApp/scriptQuestionState.js
@@ -0,0 +1,32 @@
+const questionArray = [
+  [
+    "Who is making the web standards?",
+    ["Mozilla", "Microsoft", "The World Wide Web Consortium"],
+    "The World Wide Web Consortium",
+  ],
+  [
+    "What is the correct HTML for creating a hyperlink?",
+    ["Itonlineclub.tk", "Itonline.pk", "Itonline.sk", "Http://Itonlineclub.pk"],
+    "Itonlineclub.tk",
+  ],
+  [
+    "How can you create an e-mail link",
+    ["Csa", "Xxx@yyy", "Cas", "Das"],
+    "Cas",
+  ],
+  [
+    "How can you open a link in a new browser window",
+    ["_blank", "Target", "Same", "Open"],
+    "_blank",
+  ],
+  [
+    "What does HTML stands for?",
+    [
+      "Hypertext and Markup Language",
+      "Hyperlinks",
+      "Hyperlinks and Markup Language",
+      "Hometool and markup language",
+    ],
+    "Hypertext and Markup Language",
+  ],
+];
