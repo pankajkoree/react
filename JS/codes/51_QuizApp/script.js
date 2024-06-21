e48581e3b0515ee02333f4529a1b6f6411da9eeb
failedCoderrr <pankajkori2057@gmail.com>
2024-06-21 22:03:58 +0545

    Quiz App

--- JS/codes/51_QuizApp/script.js
+++ JS/codes/51_QuizApp/script.js
@@ -9,6 +9,22 @@ newAccount.addEventListener("click", () => {
   formDiv.classList.add("close");
 });
 
+// adding the value to localstorage i.e. username and password
+const userIDNewAccount = document.querySelector("#userid_new_account");
+const passwordNewAccount = document.querySelector("#password_new_account");
+
+let iDNewAccount;
+let pwdNewAccount;
+
+userIDNewAccount.addEventListener("input", (e) => {
+  iDNewAccount = e.target.value;
+});
+passwordNewAccount.addEventListener("input", (e) => {
+  pwdNewAccount = e.target.value;
+});
+
+let userDetailsFromNewAccount = {};
+
 newAccountButton.addEventListener("click", () => {
   newAccountForm.classList.remove("open");
   formDiv.classList.remove("close");
