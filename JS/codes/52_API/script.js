079148b14370003fed31819d776ac35cc8d5f3f9
failedCoderrr <pankajkori2057@gmail.com>
2024-06-22 11:25:41 +0545

    API

---
+++ JS/codes/52_API/script.js
@@ -0,0 +1,7 @@
+// adding json values in js file, treats it as object thats why key aren't having double quotes but if the same thing will be in JSON, each and everything should be inside double quotes
+const user = {
+  userId: 1,
+  id: 1,
+  title: "delectus aut autem",
+  completed: false,
+};
