903da07516ca06b14b13936d692fb58f416e543d
failedCoderrr <pankajkori2057@gmail.com>
2024-06-23 21:50:29 +0545

    promise

---
+++ JS/codes/56_promises/script.js
@@ -0,0 +1,12 @@
+// making promises
+
+// const prom = new Promise(); // throws the error normally , coz we need to pass a function inside it so
+
+// ----------------------------------------------------
+
+// lets pass the function
+// const prom = new Promise(() => {}); // promise can have three state, i.e. pending, resolved, and rejected, but as of now it will pe pending, we need to pass again some argument
+
+// ----------------------------------------------------
+
+const prom = new Promise(() => {});
