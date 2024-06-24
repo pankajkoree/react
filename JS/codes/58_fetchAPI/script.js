82736bb5cb2f18337cc0e817f8813013c0c094fd
failedCoderrr <pankajkori2057@gmail.com>
2024-06-24 20:08:24 +0545

    fetch API

--- JS/codes/58_fetchAPI/script.js
+++ JS/codes/58_fetchAPI/script.js
@@ -36,11 +36,27 @@
 
 // ----------------------------------------------------------
 
-fetch("https://dummyjson.com/products", {
-  method: "POST",
-})
-  .then((res) => {
-    //res is the response which we get when promise gets resolved
-    return res.json();
-  })
-  .then((data) => console.log(data));
+// fetch("https://dummyjson.com/products", {
+//   method: "POST", //request method can be anything which means it can custom and this link doesn't support POST method
+// })
+//   .then((res) => {
+//     //res is the response which we get when promise gets resolved
+//     return res.json();
+//   })
+//   .then((data) => console.log(data));
+
+// ----------------------------------------------------------
+
+// fetch("https://dummyjson.com/products/add", {
+//   method: "POST",
+//   headers: { "Content-Type": "application/json" }, // header are optional
+//   body: JSON.stringify({
+//     title: "BMW Pencil",
+//     category: "Stationary",
+//     /* other product data */
+//   }),
+// })
+//   .then((res) => res.json())
+//   .then(console.log);
+
+// ----------------------------------------------------------
