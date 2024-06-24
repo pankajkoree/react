0d8e02567dc4c7bf0ecb2763be619e506f09feeb
failedCoderrr <pankajkori2057@gmail.com>
2024-06-24 19:45:23 +0545

    fetch API

--- JS/codes/58_fetchAPI/script.js
+++ JS/codes/58_fetchAPI/script.js
@@ -13,9 +13,34 @@
 
 // ----------------------------------------------------------
 
-fetch("https://dummyjson.com/products").then((res) => {
-  //res is the response which we get when promise gets resolved
-  res.json().then((data) => {
-    console.log(data);
-  });
-});
+// by default it sends the get request
+
+// fetch("https://dummyjson.com/products")
+//   .then((res) => {
+//     //res is the response which we get when promise gets resolved
+//     return res.json();
+//   })
+//   .then((data) => console.log(data));
+
+// ----------------------------------------------------------
+
+// fetch("https://dummyjson.com/products", {
+//   // this url doesn't support post request so we get error
+//   method: "POST",
+// })
+//   .then((res) => {
+//     //res is the response which we get when promise gets resolved
+//     return res.json();
+//   })
+//   .then((data) => console.log(data));
+
+// ----------------------------------------------------------
+
+fetch("https://dummyjson.com/products", {
+  method: "POST",
+})
+  .then((res) => {
+    //res is the response which we get when promise gets resolved
+    return res.json();
+  })
+  .then((data) => console.log(data));
