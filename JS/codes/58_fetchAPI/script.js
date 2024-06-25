5012e8170068b32aeb416c841ea5209fa31e4046
failedCoderrr <pankajkori2057@gmail.com>
2024-06-25 09:00:11 +0545

    fetch API

--- JS/codes/58_fetchAPI/script.js
+++ JS/codes/58_fetchAPI/script.js
@@ -61,28 +61,67 @@
 
 // ----------------------------------------------------------
 // function makeFetchRequest(url) {
-//     return fetch(url)
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error(`HTTP error! Status: ${response.status}`);
-//         }
-//         return response.json();
-//       });
-//   }
+//   return fetch(url).then((response) => {
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     return response.json();
+//   });
+// }
 
-//   makeFetchRequest("https://dummyjson.com/users")
-//     .then(usersData => {
-//       return makeFetchRequest(`https://dummyjson.com/posts/user/${usersData.users[0].id}`);
-//     })
-//     .then(postsData => {
-//       return makeFetchRequest(`https://dummyjson.com/comments/post/${postsData.posts[0].id}`);
-//     })
-//     .then(commentsData => {
-//       return makeFetchRequest(`https://dummyjson.com/users/${commentsData.comments[0].user.id}`);
-//     })
-//     .then(userData => {
-//       console.log(userData);
-//     })
-//     .catch(error => {
-//       console.error('Error:', error);
-//     });
+// makeFetchRequest("https://dummyjson.com/users")
+//   .then((usersData) => {
+//     return makeFetchRequest(
+//       `https://dummyjson.com/posts/user/${usersData.users[0].id}`
+//     );
+//   })
+//   .then((postsData) => {
+//     return makeFetchRequest(
+//       `https://dummyjson.com/comments/post/${postsData.posts[0].id}`
+//     );
+//   })
+//   .then((commentsData) => {
+//     return makeFetchRequest(
+//       `https://dummyjson.com/users/${commentsData.comments[0].user.id}`
+//     );
+//   })
+//   .then((userData) => {
+//     console.log(userData);
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
+
+// ------------------------------------------------------------------
+
+function fetchDetails(url) {
+  return fetch(url).then((res) => {
+    return res.json();
+  });
+}
+
+fetchDetails("https://dummyjson.com/users")
+  .then((userData) => {
+    return fetchDetails(
+      `https://dummyjson.com/posts/user/${userData.users[0].id}`
+    );
+  })
+  .then((postsData) => {
+    return fetchDetails(
+      `https://dummyjson.com/comments/post/${postsData.posts[0].id}`
+    );
+  })
+  .then((commentsData) => {
+    return fetchDetails(
+      `https://dummyjson.com/users/${commentsData.comments[0].user.id}`
+    );
+  })
+  .then((user) => {
+    console.log("Id : ", user.id);
+
+    console.log("FirstName : ", user.firstName);
+    console.log("Age : ", user.age);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
