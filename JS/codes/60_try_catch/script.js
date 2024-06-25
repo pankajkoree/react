2a0cd65b77329f4fac0a47b7d8006c9b12c0b911
failedCoderrr <pankajkori2057@gmail.com>
2024-06-25 19:49:08 +0545

    try and catch

---
+++ JS/codes/60_try_catch/script.js
@@ -0,0 +1,11 @@
+// ---------------Try and Catch ------------------------
+
+try {
+  console.log(a);
+} catch (err) {
+  console.log(err); //error is a object
+}
+
+console.log(5 + 67);
+
+// ------------------------------------------------------
