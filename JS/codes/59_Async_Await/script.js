f5e3d8950d09a147cb2dbe756a113869117089bd
failedCoderrr <pankajkori2057@gmail.com>
2024-06-25 09:47:47 +0545

    Async and Await

--- JS/codes/59_Async_Await/script.js
+++ JS/codes/59_Async_Await/script.js
@@ -23,6 +23,33 @@
 
 // --------------------------------------------------------
 
-async function makeRequest() {}
+// async function makeRequest() {
+//   // there's a keyword : throw
+//   // which means throw the error
+//   // anything after the throw statement won't be execute as below
+
+//   console.log("before throw");
+
+//   throw "nothing will be executed after this"; // it is also as return statement, which means the result here will be the throw statement
+
+//   console.log("after throw"); //Unreachable code detected
+// }
 
 // --------------------------------------------------------
+
+// function hi() {
+//   // in detail throw error is as:
+//   console.log("before detail error");
+
+//   throw new Error("detailed error"); //here the Error is the error constructor which is inbuilt
+
+//   console.log("after detail error");
+// }
+
+// --------------------------------------------------------
+
+// rejecting a promise using async
+
+async function makeRequest() {
+  throw new Error("error occurred");
+}
