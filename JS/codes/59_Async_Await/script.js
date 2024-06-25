d47f0d06a6d3d0eb326d27f64abdca7628cfb8b9
failedCoderrr <pankajkori2057@gmail.com>
2024-06-25 09:36:42 +0545

    Async and await

---
+++ JS/codes/59_Async_Await/script.js
@@ -0,0 +1,28 @@
+// -----------------Async and Await------------------------
+
+// function makeRequest() {
+//   // by default if we won't return anything, the function returns undefined but if we make a function async it will return a promise as below
+//   return 1;
+// }
+
+// --------------------------------------------------------
+
+// async function makeRequest() {}
+
+// by default, the output would be  as :
+// Promise {<fulfilled>: undefined}
+// [[Prototype]]
+// :
+// Promise
+// [[PromiseState]]
+// :
+// "fulfilled"
+// [[PromiseResult]]
+// :
+// undefined
+
+// --------------------------------------------------------
+
+async function makeRequest() {}
+
+// --------------------------------------------------------
