a9e9fe78bd18db8d0cc48a45c8c01d73ef701240
failedCoderrr <pankajkori2057@gmail.com>
2024-06-25 11:43:52 +0545

    async and await

--- JS/codes/59_Async_Await/script.js
+++ JS/codes/59_Async_Await/script.js
@@ -58,22 +58,72 @@
 
 //getting the data after 5 seconds of delay
 
+// async function makeRequest() {
+//   setTimeout(async () => {
+//     const url = "https://dummyjson.com/users";
+
+//     const responsePromise = await fetch(url); // await returns the result of the fetch
+
+//     console.log(responsePromise);
+
+//     console.log("hi");
+//   }, 5000);
+
+//   //   responsePromise.then((res) => res.json());
+// }
+
+// // this makes the code to behave like synchronous code, which means code will be executed line by line
+// // if we'd used then, the hi would have run before the resonse comes
+// // but its actually an asynchronous code
+
+// makeRequest();
+
+// --------------------------------------------------------
+
+// adding another function to test that it doesn't block the execution of other although it waits for the response
+// it doesn't use the browsers main thred so it doesn't freezes the content
+// async function makeRequest() {
+//   setTimeout(async () => {
+//     const url = "https://dummyjson.com/users";
+
+//     const responsePromise = await fetch(url); // await returns the result of the fetch
+
+//     console.log(responsePromise);
+
+//     console.log("hi");
+//   }, 5000);
+
+//   //   responsePromise.then((res) => res.json());
+// }
+
+// function addTwoNumber() {
+//   return 5 + 6;
+// }
+
+// --------------------------------------------------------
+// fetching the acutal data
+// like we used to have a error control in .then statement but what about here, for that thing we need try and catch
 async function makeRequest() {
   setTimeout(async () => {
     const url = "https://dummyjson.com/users";
 
     const responsePromise = await fetch(url); // await returns the result of the fetch
 
-    console.log(responsePromise);
+    const data = await responsePromise.json();
 
-    console.log("hi");
+    // // the above two lines code can be done in single line using await again as :
+    // const data = await (await fetch(url)).json(); // await returns the result of the fetch
+    // // but its not the recommended way to do
+
+    console.log(data);
   }, 5000);
+}
+makeRequest();
 
-  //   responsePromise.then((res) => res.json());
+function addTwoNumber() {
+  return 5 + 6;
 }
 
-// this makes the code to behave like synchronous code, which means code will be executed line by line
-// if we'd used then, the hi would have run before the resonse comes
-// but its actually an asynchronous code
-
-makeRequest();
+// ---------------------------------------------
+// generally we can't keep await outside the async function
+// but if we want to keep outside, we need type="module" in js linking
