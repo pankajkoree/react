fc3a7fd28f6798c4e03fb7f5c4047f47d422b477
failedCoderrr <pankajkori2057@gmail.com>
2024-06-27 12:02:56 +0545

    import and export

--- JS/codes/62_import_export/script.js
+++ JS/codes/62_import_export/script.js
@@ -1,13 +1,35 @@
 // -----------import and export -----------------------------
 
-import { usersData } from "./usersData.js";
+// import { usersData } from "./usersData.js";
 // import { productsData } from "./productsData.js"; //default export can't be imported using {} so
 import productsData from "./productsData.js";
-import getFirstUser from "./usersData.js";
+// import getFirstUser from "./usersData.js";
 
-console.log(usersData);
+// console.log(usersData);
 console.log(productsData);
 
-console.log(getFirstUser());
+// console.log(getFirstUser());
 
 // ---------------------------------------------------------
+
+// import { usersData } from "./usersData.js";
+// import getFirstUser from "./usersData.js";
+
+// these both things can be import as whole as
+import * as userData from "./usersData.js";
+
+console.log(userData);
+
+// the default one can be used as
+// console.log(userData.default);  // the output will be as : getFirstUser() {
+//     return usersData[0];
+//   }
+
+// ---------------------------------------------------------
+
+// but if default export is a function we can directly access it as
+
+console.log(userData.default()); // we'll get the actual result
+
+// and other thing can be accessd using . operator
+console.log(userData.usersData); // we get the actual result
