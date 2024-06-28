5078510ec97d2b75101cf0c3c31b6f70131bed82
failedCoderrr <pankajkori2057@gmail.com>
2024-06-28 09:40:14 +0545

    constrcutor and new keyword

--- JS/codes/64_constructor_newKeyword/script.js
+++ JS/codes/64_constructor_newKeyword/script.js
@@ -28,19 +28,80 @@
 // changing user.age to this.age
 // this normally points to window object but when used with some other,it points to that
 
-function getAgeYear() {
-  return new Date().getFullYear() - this.age;
-}
+// function getAgeYear() {
+//   return new Date().getFullYear() - this.age;
+// }
+
+// function createUser(firstName, lastName, age) {
+//   const user = {
+//     firstName,
+//     lastName,
+//     age,
+//     getAgeYear,
+//   };
+//   return user;
+// }
+
+// const user1 = createUser("Pk", "l", 22);
+// const user2 = createUser("Ak", "m", 23);
+
+// const a1 = [1, 2];
+// const a2 = [3, 4];
+
+// a1.pop === a2.pop;
+
+// user1.getAgeYear === user2.getAgeYear
+// here this comparision gives true result, which means their memory address are same
+// but it also means the polymorphism, which means that have more than one form
+
+// ---------------------------------------------------------
+// using common method i.e. commonMethods
+
+// function createUser(firstName, lastName, age) {
+//   const user = {
+//     firstName,
+//     lastName,
+//     age,
+//     getAgeYear: createUser.commonMethods.getAgeYear,
+//   };
+//   return user;
+// }
+
+// createUser.commonMethods = {
+//   getAgeYear() {
+//     return new Date().getFullYear() - this.age;
+//   },
+// };
+
+// const user1 = createUser("Pk", "l", 22);
+// const user2 = createUser("Ak", "m", 23);
+
+// const a1 = [1, 2];
+// const a2 = [3, 4];
+
+// a1.pop === a2.pop;
+
+// user1.getAgeYear === user2.getAgeYear
+// comparing it again gives true result, which means its defined only once
+
+// above logic is good, but the problem is we need to write extra code , so we have another logic as below:
+// ---------------------------------------------------------
 
 function createUser(firstName, lastName, age) {
   const user = {
     firstName,
     lastName,
     age,
-    getAgeYear,
+    getAgeYear: createUser.commonMethods.getAgeYear,
   };
   return user;
 }
+
+createUser.commonMethods = {
+  getAgeYear() {
+    return new Date().getFullYear() - this.age;
+  },
+};
 
 const user1 = createUser("Pk", "l", 22);
 const user2 = createUser("Ak", "m", 23);
@@ -49,7 +110,3 @@ const a1 = [1, 2];
 const a2 = [3, 4];
 
 a1.pop === a2.pop;
-
-// user1.getAgeYear === user2.getAgeYear
-// here this comparision gives true result, which means their memory address are same
-// but it also means the polymorphism, which means that have more than one form
