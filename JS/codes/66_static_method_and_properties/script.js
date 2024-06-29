46da7a7ba1e92f1616473ff6d8561c1dda3c77dd
failedCoderrr <pankajkori2057@gmail.com>
2024-06-29 20:20:54 +0545

    static method and properties

--- JS/codes/66_static_method_and_properties/script.js
+++ JS/codes/66_static_method_and_properties/script.js
@@ -5,13 +5,27 @@ class CreateUser {
     this.age = age;
   }
 
+  static hi = "hello"; //static properties won't directly goes into its created object like theres user1,user2
+
+  static {
+    console.log(this);
+    // console.log(
+    //   "runs automatically without even creating a object of the class"
+    // );
+
+    let a = 4; //won't be accessible outside of it
+
+    this.getFullName = function () {
+      return this.firstName + " " + this.lastName;
+    }; //this is static method
+  }
+
   getBirthYear() {
     return new Date().getFullYear() - this.age;
   }
-  getFullName() {
-    return this.firstName + " " + this.lastName;
-  }
 }
+
+// CreateUser.Myname = "pankaj"; // can also be done like this, which will be static
 
 const user1 = new CreateUser("Rohit", "Sharma", 37);
 const user2 = new CreateUser("Jasprit", "Bumrah", 30);
