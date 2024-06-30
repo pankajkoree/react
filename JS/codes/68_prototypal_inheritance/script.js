db92bae2bfa3581fc9c34b2fc2c557ad26a2a8f0
failedCoderrr <pankajkori2057@gmail.com>
2024-06-30 08:59:26 +0545

    prototypal inheritance

--- JS/codes/68_prototypal_inheritance/script.js
+++ JS/codes/68_prototypal_inheritance/script.js
@@ -103,3 +103,29 @@ class User {
 
 // const user1 = new User("Rohit", "Sharma", 37);
 // const user2 = new User("Virat", "Kohli", 36);
+
+class Student extends User {
+  // adding an extra property in constructor
+  constructor(firstName, lastName, age, course) {
+    super(firstName, lastName, age);
+    this.course = course;
+  }
+
+  study() {
+    console.log("studying.....");
+  }
+}
+const st1 = new Student("Rohit", "Sharma", 37, "BSC");
+
+class Employee extends User {
+  constructor(firstName, lastName, age, company) {
+    super(firstName, lastName, age);
+    this.company = company;
+  }
+
+  work() {
+    //if parent also has same function, then the nearest function will be called, ifnot available then only fetches parent method
+    return `${this.fullName} works at ${this.company}`;
+  }
+}
+const emp1 = new Employee("Rohit", "Sharma", 36, "Google");
