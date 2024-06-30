86130186465f424d2d280194bfc7b5b0ce78e264
failedCoderrr <pankajkori2057@gmail.com>
2024-06-30 08:29:21 +0545

    prototypal inheritance

--- JS/codes/68_prototypal_inheritance/script.js
+++ JS/codes/68_prototypal_inheritance/script.js
@@ -1,23 +1,24 @@
-const user = {
-  firstName: "Rohit",
-  lastName: "Sharma",
+class User {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
 
-  // fullName: function () {
-  //   return `${this.firstName} ${this.lastName}`;
-  // },
+  getBirhtYear() {
+    return new Date().getFullYear() - this.age;
+  }
 
   get fullName() {
-    console.log(
-      "runs when clicked on this function in console or gets accessed"
-    );
     return `${this.firstName} ${this.lastName}`;
-  },
+  }
+
   set fullName(value) {
     const [firstName, lastName] = value.split(" ");
     this.firstName = firstName;
     this.lastName = lastName;
-  },
-};
+  }
+}
 
-user.fullName = "Axar Patel"; //this will set the value too
-console.log(user.fullName);
+const user1 = new User("Rohit", "Sharma", 37);
+const user2 = new User("Virat", "Kohli", 36);
