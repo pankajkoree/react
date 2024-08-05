7c0d4f502a34610ae72b4a4ffcf06c7cd0247d7e
failedCoderrr <pankajkori2057@gmail.com>
2024-08-05 16:41:38 +0530

    day 24

--- 30-days-js-challenge/24_weatherApp/script.js
+++ 30-days-js-challenge/24_weatherApp/script.js
@@ -4,6 +4,9 @@ let apiKey = "ce81d71ef84a6c9940a141c72f07a3b9";
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?&units=metrics&q=";
 
+const fiveDayURL =
+  "https://api.openweathermap.org/data/2.5/forecast?units=metric&q=";
+
 const cityInput = document.querySelector("#searchCity");
 const searchCityButton = document.querySelector("#searchCityButton");
 const temperaturValue = document.querySelector(".temp");
@@ -16,33 +19,36 @@ async function checkWeather(city) {
   console.log(data);
   let defaultCityName = data.name;
   console.log(defaultCityName);
-  let temperature = Math.floor(data.main.temp - 273.15) + `°C`
+  let temperature = Math.floor(data.main.temp - 273.15) + `°C`;
   console.log(temperature);
-  temperaturValue.textContent = temperature
+  temperaturValue.textContent = temperature;
 
-  updateCityName.textContent = defaultCityName
-  const weatherName = document.querySelector("#weatherType")
+  updateCityName.textContent = defaultCityName;
+  const weatherName = document.querySelector("#weatherType");
 
   const weatherType = data.weather[0].main;
   if (weatherType === "Clouds") {
     weatherImage.src = "images/cloudy.png";
-    weatherName.textContent = weatherType
+    weatherName.textContent = weatherType;
   } else if (weatherType === "Rain") {
     weatherImage.src = "images/rainy.png";
-    weatherName.textContent = weatherType
+    weatherName.textContent = weatherType;
   } else {
     weatherImage.src = "images/cloudy.png";
-    weatherName.textContent = weatherName.value
+    weatherName.textContent = weatherName.value;
   }
   console.log(weatherType);
+
+  const fiveDayResponse = await fetch(fiveDayURL + city + `&appid=${apiKey} `);
+
+  const fiveDayJSOn = await fiveDayResponse.json();
+
+  console.log(fiveDayJSOn);
 }
 
 checkWeather("raipur");
 
-
-searchCityButton.addEventListener("click",() => {
-    const getCityName = cityInput.value
-    checkWeather(getCityName)
-})
-
-
+searchCityButton.addEventListener("click", () => {
+  const getCityName = cityInput.value;
+  checkWeather(getCityName);
+});
---
+++ 30-days-js-challenge/24_weatherApp/script2.js
@@ -0,0 +1,8 @@
+const newTemperature = document.querySelector(".newTemperature")
+const newCity = document.querySelector(".newCityName")
+const newWeatherImage = document.querySelector("#newImage")
+const getDate = document.querySelector("#newDate")
+const newButton = document.querySelector("#getData")
+
+import * as fiveDayData from "./script.js"
+
