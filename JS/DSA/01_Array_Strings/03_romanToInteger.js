4c339dea6c34ad1c2b8a7181457a7f2ba1c42401
failedCoderrr <pankajkori2057@gmail.com>
2024-08-05 23:29:05 +0530

    DSA

--- JS/DSA/01_Array_Strings/03_romanToInteger.js
+++ JS/DSA/01_Array_Strings/03_romanToInteger.js
@@ -10,28 +10,29 @@ var romanToInt = function (s) {
     D: 500,
     M: 1000,
   };
-  let stringSplit = s.split("");
-  console.log(stringSplit);
-  let indexes = [];
-  let result = [];
-  stringSplit.forEach((romNum) => {
-    if (romanNotaion.includes(romNum)) {
-      let valIndex = romanNotaion.indexOf(romNum);
-      indexes.push(valIndex);
-      console.log(numberNotaion[valIndex]);
+  // let stringSplit = s.split("");
+  // console.log(stringSplit);
+  // let indexes = [];
+  // let result = [];
+  // stringSplit.forEach((romNum) => {
+  //   if (romanNotaion.includes(romNum)) {
+  //     let valIndex = romanNotaion.indexOf(romNum);
+  //     indexes.push(valIndex);
 
-      for (let i = 0; i < indexes.length; i++) {
-        if (indexes[i] > indexes[i + 1]) {
-          result.push(indexes[i] + indexes[i + 1]);
-        }
-        if (indexes[i] < indexes[i + 1]) {
-          result.push(indexes[i + 1] - indexes[i]);
-        }
-      }
-    }
-  });
-  console.log(indexes);
-  console.log(result);
+  //     for (let i = 0; i < indexes.length; i++) {
+  //      console.log(indexes[i]);
+  //     }
+  //   }
+  // });
+  // console.log(indexes);
+  // console.log(result);
+  let value = 0;
+  for (let i = 0; i < s.length; i++) {
+    indicationValue[s[i]] < indicationValue[s[i + 1]]
+      ? (value -= indicationValue[s[i]])
+      : (value += indicationValue[s[i]]);
+  }
+  return value;
 };
 
-romanToInt("XIV");
+console.log(romanToInt("XIV"))
