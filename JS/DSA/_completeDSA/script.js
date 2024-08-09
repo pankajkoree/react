a24467a5a5874ab9401babd7f34c8a04cc16db0f
failedCoderrr <pankajkori2057@gmail.com>
2024-08-09 23:08:52 +0530

    DSA

--- JS/DSA/_completeDSA/script.js
+++ JS/DSA/_completeDSA/script.js
@@ -155,3 +155,9 @@ var twoSum = function (nums, target) {
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum([3, 2, 4], 6));
 console.log(twoSum([3, 3], 6));
+
+let str = "HELLO HOW ARE YOU DOING"
+console.log(str.toLocaleLowerCase());
+console.log(str);
+console.log(str.toLowerCase());
+console.log(str);
