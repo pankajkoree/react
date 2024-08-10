4356ce3f7ba3ca41db54ee0a9dd192ec15412fce
failedCoderrr <pankajkori2057@gmail.com>
2024-08-10 22:56:21 +0530

    DSA

--- JS/DSA/_completeDSA/script.js
+++ JS/DSA/_completeDSA/script.js
@@ -183,3 +183,31 @@ var strStr = function (haystack, needle) {
 
 console.log(strStr("leetcode", "leeto"));
 console.log(strStr("sadbutsad", "sad"));
+
+// valid anagrams
+var isAnagram = function (s, t) {
+  // using builtin function
+  // return s.split("").sort().join("") === t.split("").sort().join("")
+  const count = {};
+  for (let char of s) {
+    if (!count[char]) {
+      count[char] = 0;
+    }
+    count[char]++;
+  }
+  for (let char of t) {
+    if (count[char] === undefined) {
+      return false;
+    } else {
+      count[char]--;
+    }
+  }
+  for (let char in count) {
+    if (count[char] !== 0) {
+      return false;
+    }
+  }
+  return true;
+};
+console.log(isAnagram("anagram", "gramnaa"));
+console.log(isAnagram("eat", "tear"));
