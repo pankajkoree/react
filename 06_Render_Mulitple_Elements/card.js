e222972b7aa7abca19cfbcbd20d368d343cf19a8
failedCoderrr <pankajkori2057@gmail.com>
2024-08-13 14:21:15 +0530

    rendering multiple elements

--- 06_Render_Mulitple_Elements/card.js
+++ 06_Render_Mulitple_Elements/card.js
@@ -1,20 +1,19 @@
-
 function Card(key) {
-    return (
-      <div className="card" key={key}>
-        <img
-          src="https://www.cnet.com/a/img/resize/b81ab3684470df1e57cf2523ef3a7d888c0cbff7/hub/2023/09/18/031f45e3-d1b7-490d-8765-af47213b5093/iphone15-pro-79.jpg?auto=webp&fit=crop&height=576&width=768"
-          alt="iphone image"
-        />
-        <div className="card-content">
-          <h3>Iphone 13</h3>
-          <p>Apple</p>
-          <p>
-            <b>₹ 599</b>
-          </p>
-        </div>
+  return (
+    <div className="card" key={key}>
+      <img
+        src="https://www.cnet.com/a/img/resize/b81ab3684470df1e57cf2523ef3a7d888c0cbff7/hub/2023/09/18/031f45e3-d1b7-490d-8765-af47213b5093/iphone15-pro-79.jpg?auto=webp&fit=crop&height=576&width=768"
+        alt="iphone image"
+      />
+      <div className="card-content">
+        <h3>Iphone 13</h3>
+        <p>Apple</p>
+        <p>
+          <b>₹ 599</b>
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export default Card
+export default Card;
--- 06_Render_Mulitple_Elements/script.js
+++ 06_Render_Mulitple_Elements/script.js
@@ -5,11 +5,11 @@ import Card from "./card";
 const root = createRoot(document.getElementById("root"));
 
 const cards = () => {
-    const cardElements = []
+  const cardElements = [];
   for (let i = 0; i <= 5; i++) {
-    cardElements.push(<Card key={i} />)
+    cardElements.push(<Card key={i} />);
   }
-  return cardElements
+  return cardElements;
 };
 
 root.render(cards());
