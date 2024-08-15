949091f40f5f787e9d375691ee3ac34020520584
failedCoderrr <pankajkori2057@gmail.com>
2024-08-15 11:16:48 +0530

    rendering multiple elements

--- 06_Render_Mulitple_Elements/card.js
+++ 06_Render_Mulitple_Elements/card.js
@@ -1,21 +1,17 @@
 import "./style.css";
 
-
-
-function Card(key,imageURL,category,heading,price) {
-
+function Card(key, imageURL, category, heading, price) {
   return (
-    <div className="card" key={key}>
-      <img
-        src={imageURL}
-        alt="iphone image"
-      />
-      <div className="card-content">
-        <h3>{heading}</h3>
-        <p>{category}</p>
-        <p>
-          <b>${price}</b>
-        </p>
+    <div className="card-container">
+      <div className="card" key={key}>
+        <img src={imageURL} alt="iphone image" className="images" />
+        <div className="card-content">
+          <h3>{heading}</h3>
+          <p>{category}</p>
+          <p>
+            <b>${price}</b>
+          </p>
+        </div>
       </div>
     </div>
   );
--- 06_Render_Mulitple_Elements/script.js
+++ 06_Render_Mulitple_Elements/script.js
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import "./style.css"
+import "./style.css";
 import Card from "./card";
 
 const root = createRoot(document.getElementById("root"));
