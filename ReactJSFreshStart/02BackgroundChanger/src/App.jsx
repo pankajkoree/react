a479d090837712be9c76e2348298afd4eae8aa20
failedCoderrr <pankajkori2057@gmail.com>
2024-08-15 17:59:26 +0530

    backgroundchanger

--- ReactJSFreshStart/02BackgroundChanger/src/App.jsx
+++ ReactJSFreshStart/02BackgroundChanger/src/App.jsx
@@ -1,10 +1,51 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("teal");
+
   return (
     <>
-      <h1>Hello</h1>
-      <h2>Bye</h2>
+      <div className="w-full h-screen" style={{ backgroundColor: color }}>
+        <div className="relative flex w-[90%] h-16 bg-white justify-center top-[840px] left-[90px] rounded-full gap-8">
+          <button
+            className="relative border-4  h-12 top-2 px-4 rounded-full bg-red-500"
+            onClick={() => setColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className="relative border-4  h-12 top-2 px-4 rounded-full bg-blue-500"
+            onClick={() => setColor("blue")}
+          >
+            Blue
+          </button>
+          <button
+            className="relative border-4  h-12 top-2 px-4 rounded-full bg-yellow-500"
+            onClick={() => setColor("gold")}
+          >
+            Gold
+          </button>
+          <button
+            className="relative border-4  h-12 top-2 px-4 rounded-full bg-green-500"
+            onClick={() => setColor("green")}
+          >
+            Green
+          </button>
+          <button
+            className="relative border-4  h-12 top-2 px-4 rounded-full bg-purple-500"
+            onClick={() => setColor("purple")}
+          >
+            Purple
+          </button>
+          <button
+            className="relative border-4  h-12 top-2 px-4 rounded-full bg-lime-500"
+            onClick={() => setColor("lime")}
+          >
+            Lime
+          </button>
+        </div>
+      </div>
     </>
   );
 }
