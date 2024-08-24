dc0aba3d8149807db26604374976c2da13e7d975
failedCoderrr <pankajkori2057@gmail.com>
2024-08-24 18:46:18 +0530

    mega Blog appwrite

--- ReactJSFreshStart/09megaBlog/src/appwrite/auth.js
+++ ReactJSFreshStart/09megaBlog/src/appwrite/auth.js
@@ -3,11 +3,60 @@ import conf from "../conf/conf";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
-    client = new Client()
-    account =  new Account()
-    
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        //call another method
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  async login({ email, password, name }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUSer :: error", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite service :: logout :: error", error);
+    }
+  }
 }
 
-const authservice = new AuthService();
+const authService = new AuthService();
 
-export default authservice;
+export default authService;
