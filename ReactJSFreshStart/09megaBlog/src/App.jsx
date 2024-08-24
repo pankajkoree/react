2988111b909c2428f998f044a7fb88bbd3b58a29
failedCoderrr <pankajkori2057@gmail.com>
2024-08-24 23:05:59 +0530

    mega Blog appwrite

--- ReactJSFreshStart/09megaBlog/src/App.jsx
+++ ReactJSFreshStart/09megaBlog/src/App.jsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>A blog with appwrite</h1>
-    </>
-  );
+  const [loading, setLoading] = useState();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.log("error while logging...", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>{/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
