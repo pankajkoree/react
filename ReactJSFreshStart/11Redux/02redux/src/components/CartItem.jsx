a0761ec58edae4ceaafd0e1c58f545709addb9f4
failedCoderrr <pankajkori2057@gmail.com>
2024-09-01 11:50:39 +0530

    redux with react

--- ReactJSFreshStart/11Redux/02redux/src/components/CartItem.jsx
+++ ReactJSFreshStart/11Redux/02redux/src/components/CartItem.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function CartItem({ title, rating, price, quantity, imageUrl }) {
-  console.log(title);
   return (
     <div className="cart-item-container">
       <div className="cart-item">
--- ReactJSFreshStart/11Redux/02redux/src/components/Header.jsx
+++ ReactJSFreshStart/11Redux/02redux/src/components/Header.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CartIcon from "../assets/cart-icon.svg";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const cartItems = useSelector((state) => state.cartItems || []);
+  let itemCount = [];
+  cartItems.map((data) => {
+    itemCount.push(data.quantity);
+  });
+  let value = itemCount.reduce((prev, curr) => {
+    return prev + curr;
+  }, 0);
   return (
     <header>
       <div className="header-contents">
@@ -11,7 +20,7 @@ export default function Header() {
         </h1>
         <Link className="cart-icon" to="/cart">
           <img src={CartIcon} alt="cart-icon" />
-          <div className="cart-items-count">0</div>
+          <div className="cart-items-count">{value}</div>
         </Link>
       </div>
     </header>
