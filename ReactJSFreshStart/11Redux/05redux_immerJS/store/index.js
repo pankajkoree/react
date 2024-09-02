0b518996ac095625345a1bf30dc1e6b4cd392ab8
failedCoderrr <pankajkori2057@gmail.com>
2024-09-02 20:35:26 +0530

    immer js with react,redux

--- ReactJSFreshStart/11Redux/05redux_immerJS/store/index.js
+++ ReactJSFreshStart/11Redux/05redux_immerJS/store/index.js
@@ -10,6 +10,7 @@ import { wishListAddItem, wishListRemoveItem } from "./slices/wishlistSlice";
 import productReducer from "./slices/productSlice";
 import cartReducer from "./slices/cartSlice";
 import wishlistSlice from "./slices/wishlistSlice";
+import { produce } from "immer";
 
 const reducer = combineReducers({
   products: productReducer,
@@ -21,3 +22,33 @@ export const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__?.()
 );
+
+const users = [
+  {
+    name: "pankaj",
+    age: 23,
+  },
+  {
+    name: "rajan",
+    age: 20,
+  },
+  {
+    name: "ram",
+    age: 24,
+  },
+];
+
+// users[1].age = 5;
+
+// this makes a new modified array but this takes more lines of code... thats why we have produce method
+// const modifiedUser=users.map((user, i) => {
+//   return i == 1 ? { ...user, age: 5 } : user;
+// });
+
+const modifiedUser = produce(users, (usersCopy) => {
+  usersCopy[1].age = 5;
+  console.log(usersCopy);
+});
+
+console.log(modifiedUser);
+console.log(users);
