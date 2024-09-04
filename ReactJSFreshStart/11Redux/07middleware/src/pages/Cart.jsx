ac37c36e1b6ac93ac7850aa1191b7b7d70fd1bde
failedCoderrr <pankajkori2057@gmail.com>
2024-09-04 23:17:18 +0530

    redux toolkit

--- ReactJSFreshStart/11Redux/07middleware/src/pages/Cart.jsx
+++ ReactJSFreshStart/11Redux/07middleware/src/pages/Cart.jsx
@@ -3,15 +3,19 @@ import CartItem from "../components/CartItem";
 import { useSelector } from "react-redux";
 
 export default function Cart() {
+  let totalAmount = 0;
   const cartItems = useSelector(({ products, cartItems }) => {
     return cartItems.map(({ productId, quantity }) => {
       const cartProduct = products.list.find(
         (product) => product.id === productId
       );
-      return cartProduct, quantity;
+
+      return { ...cartProduct, quantity };
     });
   });
-
+  cartItems.map((element) => {
+    totalAmount += element.quantity * element.price;
+  });
   return (
     <div className="cart-container">
       <h2>Items in Your Cart</h2>
@@ -28,16 +32,16 @@ export default function Cart() {
             productId={data.id}
             title={data.title}
             price={data.price}
-            quantity={cartItems.quantity}
+            quantity={data.quantity}
             imageUrl={data.image}
-            rating={data.rating}
+            rating={data.rating.rate}
           />
         ))}
         <div className="cart-header cart-item-container">
           <div></div>
           <div></div>
           <div></div>
-          <div className="total">${}</div>
+          <div className="total">${totalAmount}</div>
         </div>
       </div>
     </div>
