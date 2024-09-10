d9bb9eb64e4d788131ed65c26d0f132c05723602
failedCoderrr <pankajkori2057@gmail.com>
2024-09-10 20:57:43 +0530

    03NEXTJS Link vs Navigation and Nested Routing

--- NextJS/03routing/src/app/contact/page.js
+++ NextJS/03routing/src/app/contact/page.js
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
 
@@ -6,14 +7,24 @@ const page = () => {
   const router = useRouter();
   return (
     <>
-      <h1>contact us</h1>
-      {/* routing using navigation */}
-      <button
-        onClick={() => router.push("/home")}
-        className="relative bg-gray-400 p-4 top-8 left-2"
-      >
-        Back to Home
-      </button>
+      <div className="relative flex flex-col gap-4">
+        <h1>contact us</h1>
+
+        <Link
+          href="/contact/company"
+          className="relative bg-gray-400 p-4 top-8 left-2 w-[200px]"
+        >
+          Contact company
+        </Link>
+
+        {/* routing using navigation */}
+        <button
+          onClick={() => router.push("/home")}
+          className="relative bg-gray-400 p-4 top-8 left-2 w-[200px]"
+        >
+          Back to Home
+        </button>
+      </div>
     </>
   );
 };
