269debcf3945db02eac05bb7b9beb8a54213a452
failedCoderrr <pankajkori2057@gmail.com>
2024-09-14 23:25:10 +0530

    09NEXTJS fecth api client

--- NextJS/09fetchapiclient/src/app/userlist/page.jsx
+++ NextJS/09fetchapiclient/src/app/userlist/page.jsx
@@ -19,7 +19,10 @@ const page = () => {
       <h1>Fetch API Clientside</h1>
 
       {users.map((user) => (
-        <div key={user.id} className="relative bg-blue-700 text-white w-[400px] p-2 border">
+        <div
+          key={user.id}
+          className="relative bg-blue-700 text-white w-[400px] p-2 border"
+        >
           <h2>Name : {user.name}</h2>
           <h3>Username : {user.username}</h3>
           <p>Email : {user.email}</p>
