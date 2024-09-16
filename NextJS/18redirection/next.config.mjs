ca5467c14944481bb55772d0bc481fc050e89076
failedCoderrr <pankajkori2057@gmail.com>
2024-09-17 00:23:20 +0530

    18NEXTJS redirection

--- NextJS/18redirection/next.config.mjs
+++ NextJS/18redirection/next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      //   {
+      //     // as the permanent is false it will redirect to
+      //     source: "/redirection",
+      //     destination: "/userlist",
+      //     permanent: false,
+      //   },
+
+      // last way,best way to redirect
+      {
+        source: "/redirection/:id",
+        destination: "/userlist",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
