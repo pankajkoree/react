439a4707eaf65e74d92b3bfb2e4b46339721ec61
failedCoderrr <pankajkori2057@gmail.com>
2024-09-19 21:18:40 +0530

    26NEXTJS put api

--- NextJS/26putapi/src/app/api/carts/[productId]/route.tsx
+++ NextJS/26putapi/src/app/api/carts/[productId]/route.tsx
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { carts } from "../../../../db/db";
+import { Result } from "postcss";
 
 export const GET = (req: any, value: any) => {
   const productData = carts.filter((product) => {
@@ -25,4 +26,23 @@ export const DELETE = (req: any, res: any) => {
       { result: "product not found...", success: false },
       { status: 404 }
     );
+};
+
+export const PUT = async (req: any, res: any) => {
+  // let productId = res.params.productId
+  let payload = await req.json();
+  // console.log(payload);
+  // return NextResponse.json({ success: true });
+  payload.productId = res.params.productId;
+
+  if (!payload.productId) {
+    return NextResponse.json({
+      result: "Error",
+      success: false,
+    });
+  }
+  return NextResponse.json({
+    Result: "Success",
+    success: true,
+  });
 };
