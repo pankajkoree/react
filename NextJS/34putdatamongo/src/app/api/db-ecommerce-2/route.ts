ae1b327055f02e54a952377ce643e76516172f1d
failedCoderrr <pankajkori2057@gmail.com>
2024-09-22 10:55:55 +0530

    34NEXTJS put data mongo

---
+++ NextJS/34putdatamongo/src/app/api/db-ecommerce-2/route.ts
@@ -0,0 +1,42 @@
+import { connectionString } from "@/lib/database/db_connection";
+import { Ecommerce } from "@/lib/database/model/ecommerce";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+// Reuse Mongoose connection
+let isConnected = false;
+
+const connectToDatabase = async () => {
+  if (!isConnected) {
+    await mongoose.connect(connectionString);
+    isConnected = true; // Set flag to true once connected
+  }
+};
+
+// GET Handler
+export const GET = async () => {
+  await connectToDatabase();
+
+  const cartData = await Ecommerce.find();
+
+  return NextResponse.json({ result: cartData });
+};
+
+// POST Handler
+export const POST = async (req: any) => {
+  const payload = await req.json();
+
+  // Convert string values to numbers
+  const cartData = new Ecommerce({
+    ...payload,
+    price: Number(payload.price),
+    quantity: Number(payload.quantity),
+    total: Number(payload.total),
+  });
+
+  await connectToDatabase();
+
+  const response = await cartData.save();
+
+  return NextResponse.json({ result: response, success: true });
+};
