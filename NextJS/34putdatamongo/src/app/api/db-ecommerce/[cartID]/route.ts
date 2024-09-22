fa7dc73632bf54b5e39c276b6c97903a7afbcd6d
failedCoderrr <pankajkori2057@gmail.com>
2024-09-22 11:45:43 +0530

    34NEXTJS put data mongo

---
+++ NextJS/34putdatamongo/src/app/api/db-ecommerce/[cartID]/route.ts
@@ -0,0 +1,23 @@
+import { connectionString } from "@/lib/database/db_connection";
+import { Ecommerce } from "@/lib/database/model/ecommerce";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+export const PUT = async (req: any, res: any) => {
+  // <!---------fetching id---------!>
+  const getCartId = res.params.cartID;
+  const id = { _id: getCartId };
+  //   console.log(id);
+  // <!---------END---------!>
+
+  // <!---------fetching payload---------!>
+  const payload = await req.json();
+  //   console.log(payload);
+
+  // <!---------END---------!>
+
+  await mongoose.connect(connectionString);
+  const result = await Ecommerce.findOneAndUpdate(id, payload);
+
+  return NextResponse.json({ result, success: true });
+};
