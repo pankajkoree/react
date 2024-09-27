c59b6cdb0dc75c45385822472d4bcc3798c30a6c
failedCoderrr <pankajkori2057@gmail.com>
2024-09-28 01:13:25 +0530

    43NEXTJS connection to database

---
+++ NextJS/43ecommercesecond/src/app/api/db-ecommerce/route.js
@@ -0,0 +1,9 @@
+import { connectionString } from "@/lib/database/db_connection";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  await mongoose.connect(connectionString);
+
+  return NextResponse.json({ result: "Success" });
+};
---
+++ NextJS/43ecommercesecond/src/lib/database/db_connection.js
@@ -0,0 +1,8 @@
+const username = process.env.dbusername;
+const password = process.env.dbpassword;
+
+if (!username || !password) {
+  throw new Error("Missing database credentials");
+}
+
+export const connectionString = `mongodb+srv://${username}:${password}@cluster0.u9aqx.mongodb.net/myDatabase?retryWrites=true&w=majority`;
