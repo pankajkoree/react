b4ab57ea7f29e2b817d4374d2b5efc2eecd9748a
failedCoderrr <pankajkori2057@gmail.com>
2024-10-02 00:02:44 +0530

    signup , verifyEmail

--- NextJS/44ecommerce/src/app/signup/page.jsx
+++ NextJS/44ecommerce/src/app/signup/page.jsx
@@ -1,95 +1,91 @@
 "use client";
 
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "react-hot-toast";
 
-const AdminLogin = () => {
-  const [userType, setUserType] = useState("");
-  const [email, setEmail] = useState(""); // Use email for consistency
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+const Signup = () => {
+  const router = useRouter();
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault(); // Corrected typo
+  const onSignUp = async (e) => {
+    e.preventDefault();
 
     try {
-      const response = await fetch("/api/admin/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json", // Ensure correct headers
-        },
-        body: JSON.stringify({ email, password }),
-      });
-      const data = await response.json();
-
-      if (data.success) {
-        alert("Login successful");
-      } else {
-        alert(data.message);
-      }
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      toast.success("Successfully signed up");
+      router.push("/login");
     } catch (error) {
-      alert("Login error: " + error.message);
+      toast.error("Signup failed : ", error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
+  useEffect(() => {
+    if (
+      user.username.length > 0 &&
+      user.email.length > 0 &&
+      user.password.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
   return (
     <div className="relative flex flex-col justify-center items-center w-[30%] left-[35%] top-5 text-2xl border-2 border-yellow-300 rounded-lg">
-      <h1>Signup Page</h1>
+      <h1>{loading ? "Processing" : " Signup"}</h1>
 
-      <form
-        className="relative flex flex-col justify-center p-5 gap-4 w-full"
-        onSubmit={handleSubmit}
-      >
-        <Label className="relative flex text-xl left-[5%]">User Type</Label>
+      <form className="relative flex flex-col justify-center p-5 gap-4 w-full">
+        <Label className="relative flex text-xl left-[5%]">Username</Label>
         <Input
           type="text"
-          placeholder="usertype"
-          value={userType}
+          placeholder="username"
+          value={user.user}
           className="relative left-[5%] w-[90%] text-xl"
-          onChange={(e) => setUserType(e.target.value)}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
         />
         <Label className="relative flex text-xl left-[5%]">Email</Label>
         <Input
           type="email"
           placeholder="email"
-          value={email}
+          value={user.email}
           className="relative left-[5%] w-[90%] text-xl"
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
         />
         <Label className="relative flex text-xl left-[5%]">Password</Label>
         <Input
           type="password"
           placeholder="password"
-          value={password}
+          value={user.password}
           className="relative left-[5%] w-[90%] text-xl"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Label className="relative flex text-xl left-[5%]">
-          Confirm Password
-        </Label>
-        <Input
-          type="password"
-          placeholder="password"
-          value={confirmPassword}
-          className="relative left-[5%] w-[90%] text-xl"
-          onChange={(e) => setConfirmPassword(e.target.value)}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
         />
         <Button
           className="relative left-[5%] w-[90%] text-xl"
           variant="default"
           size="lg"
           type="submit"
+          onClick={onSignUp}
         >
-          Signup
+          {buttonDisabled ? "No signup" : "Signup"}
         </Button>
-        {/* <p className="relative text-center text-xl hover: cursor-pointer">
-          Forgot password ?{" "}
-        </p> */}
       </form>
     </div>
   );
 };
 
-export default AdminLogin;
+export default Signup;
