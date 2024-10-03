a48ec0cc875d9550698a4bb73ea1f685f8ee1b6e
failedCoderrr <pankajkori2057@gmail.com>
2024-10-03 23:29:08 +0530

    ecommerce

---
+++ NextJS/44ecommerce/src/middleware.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export const middleware = (request) => {
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath =
+    path === "/login" || path === "/signup" || path === "/verifyEmail";
+
+  const token = request.cookies.get("token")?.value || "";
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+};
+
+export const config = {
+  matcher: ["/", "/login", "/signup", "/verifyEmail", "/profile"],
+};
