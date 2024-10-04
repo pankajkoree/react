9c7921cc1969377819911799ee9938e5c9381963
failedCoderrr <pankajkori2057@gmail.com>
2024-10-05 00:40:33 +0530

    product model

--- NextJS/44ecommerce/src/models/productModel.js
+++ NextJS/44ecommerce/src/models/productModel.js
@@ -1,18 +1,31 @@
+import { Weight } from "lucide-react";
 import mongoose from "mongoose";
-import { number, string } from "zod";
 
 const productSchema = new mongoose.Schema({
-  id: number,
-  price: number,
+  id: String,
+  price: String,
+  title: String,
   description: String,
   category: String,
-  rating: number | String,
-  discountPercentage: number | String,
-  stock: number,
+  rating: String,
+  discountPercentage: String,
+  stock: String,
+  tags: String,
+  brand: String,
+  sku: String,
+  Weight: String,
+  dimensions: String,
   warrantyInformation: String,
   shippingInformation: String,
+  availabilityStatys: String,
   reviews: Array,
   returnPolicy: String,
   thumbnail: String,
   images: Array,
+  meta: Object,
 });
+
+const Product =
+  mongoose.models.productsData || mongoose.model("productsData", productSchema);
+
+export default Product;
