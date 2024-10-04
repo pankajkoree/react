25d58d7d9bd2cd2ce38c6e75820c0a4ca505e52f
failedCoderrr <pankajkori2057@gmail.com>
2024-10-04 15:48:39 +0530

    ecommerc

---
+++ NextJS/44ecommerce/src/app/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='relative h-12 bg-yellow-300'>
+      <h1>Footer Section</h1>
+    </div>
+  )
+}
+
+export default Footer
--- NextJS/44ecommerce/src/app/page.js
+++ NextJS/44ecommerce/src/app/page.js
@@ -1,10 +1,12 @@
 import "../../src/app/globals.css";
+import Footer from "./components/Footer";
 import NavigationBar from "./components/NavigationBar";
 
 export default function Home() {
   return (
     <>
       <NavigationBar />
+      <Footer />
     </>
   );
 }
