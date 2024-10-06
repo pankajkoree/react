875d18c46efe155520e302331e26f389f5a3e482
failedCoderrr <pankajkori2057@gmail.com>
2024-10-06 12:49:34 +0530

    products fetch

--- NextJS/44ecommerce/src/app/components/UserView.jsx
+++ NextJS/44ecommerce/src/app/components/UserView.jsx
@@ -1,16 +1,53 @@
-import React from "react";
-import toast from "react-hot-toast";
+"use client";
+
+import { useEffect, useState } from "react";
+
+const getProductsData = async () => {
+  try {
+    let response = await fetch(
+      "http://localhost:3000/api/products/getProducts"
+    );
+    response = await response.json();
+    if (response.success) {
+      return response.results;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
 
 const UserView = () => {
-  try {
-    // await fetch("")
-  } catch (error) {
-    toast.error("fetching data failed");
-  }
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getProductsData();
+      setProducts(data);
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+
+  products?.map((product) => console.log(product.title));
 
   return (
     <div>
       <h1>Page seen to user</h1>
+      {products && products.length > 0 ? (
+        <ul>
+          {products?.map((product) => (
+            <div>
+              <li>${product.title}</li>
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <p>No products available</p>
+      )}
     </div>
   );
 };
