057886148272531ebf53966139d43eb803d31b39
failedCoderrr <pankajkori2057@gmail.com>
2024-10-07 21:36:58 +0530

    NavigationBar

--- NextJS/44ecommerce/src/app/components/NavigationBar.jsx
+++ NextJS/44ecommerce/src/app/components/NavigationBar.jsx
@@ -11,12 +11,24 @@ import cartBlack from "../assets/cart-black.png";
 import cartColor from "../assets/cart-color.png";
 import userBlack from "../assets/user-black.png";
 import userColor from "../assets/user-color.png";
+import darkMode from "../assets/dark-mode.png";
+import lightMode from "../assets/light-mode.png";
 
 const NavigationBar = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  const addDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
   useEffect(() => {
-    // Check if we are in the browser and then check for dark mode
+    if (isDarkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
+  useEffect(() => {
     if (typeof window !== "undefined") {
       setIsDarkMode(document.body.classList.contains("dark"));
     }
@@ -67,8 +79,27 @@ const NavigationBar = () => {
         )}
       </div>
 
-      {/* div to have login, cart */}
+      {/* div to have,theme, login, cart */}
       <div className="relative flex flex-row justify-end items-center right-12 gap-12">
+        {/* theme div */}
+        <div
+          className="relative flex flex-row items-center gap-2"
+          onClick={addDarkMode}
+        >
+          {isDarkMode ? (
+            <Image
+              src={darkMode}
+              alt="cart"
+              className="relative w-[36px] h-[36px]"
+            />
+          ) : (
+            <Image
+              src={lightMode}
+              alt="cart"
+              className="relative w-[36px] h-[36px]"
+            />
+          )}
+        </div>
         {/* cart div */}
         <div className="relative flex flex-row items-center gap-2">
           {isDarkMode ? (
