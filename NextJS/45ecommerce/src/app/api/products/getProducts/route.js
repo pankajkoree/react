8cc34d64504b73ffe7c46f1d2b12368d95884082
failedCoderrr <pankajkori2057@gmail.com>
2024-10-11 21:38:48 +0530

    modified get product api for pagination

--- NextJS/45ecommerce/src/app/api/products/getProducts/route.js
+++ NextJS/45ecommerce/src/app/api/products/getProducts/route.js
@@ -1,21 +1,64 @@
+// fetching all data at once
+
+// import { connect } from "@/dbConfig/dbConfig";
+// import Product from "@/models/productModel";
+// import { NextResponse } from "next/server";
+
+// connect();
+
+// export const GET = async () => {
+//   try {
+//     console.log("Fetching products data...");
+//     const productsData = await Product.find();
+//     if (!productsData) {
+//       return NextResponse.json(
+//         { error: "Product not available" },
+//         { status: 400 }
+//       );
+//     }
+
+//     return NextResponse.json({ results: productsData, success: true });
+//   } catch (error) {
+//     return NextResponse.json({ error: error.message }, { status: 500 });
+//   }
+// };
+
+// adding pagination technique and limiting the number of products
+
 import { connect } from "@/dbConfig/dbConfig";
 import Product from "@/models/productModel";
 import { NextResponse } from "next/server";
 
 connect();
 
-export const GET = async () => {
+export const GET = async (req) => {
   try {
+    const { searchParams } = new URL(req.url);
+    const page = parseInt(searchParams.get("page")) || 0;
+    const limit = parseInt(searchParams.get("limit")) || 21;
+
     console.log("Fetching products data...");
-    const productsData = await Product.find();
-    if (!productsData) {
+
+    const skip = page * limit;
+
+    const productsData = await Product.find().skip(skip).limit(limit);
+
+    const totalProducts = await Product.countDocuments();
+
+    if (!productsData || productsData.length === 0) {
       return NextResponse.json(
-        { error: "Product not available" },
+        { error: "Products not available", success: false },
         { status: 400 }
       );
     }
 
-    return NextResponse.json({ results: productsData, success: true });
+    return NextResponse.json({
+      results: productsData,
+      total: totalProducts,
+      page,
+      limit,
+      success: true,
+    });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
