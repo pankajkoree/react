ceb8a32c3faf3338d6f38834d11c0f8516d6dd48
failedCoderrr <pankajkori2057@gmail.com>
2024-10-14 00:20:21 +0530

    middleware

--- NextJS/45ecommerce/src/middleware.js
+++ NextJS/45ecommerce/src/middleware.js
@@ -3,12 +3,18 @@ import { NextResponse } from "next/server";
 export const middleware = (request) => {
   const path = request.nextUrl.pathname;
 
-  console.log(path);
+  console.log(`Path: ${path}`);
 
-  // You can return a NextResponse as needed, or just return null
+  if (path === "/carts") {
+    // keep a condition for the login state to be maintainer if the user is login direct user to carts else redirect to login
+    const loginUrl = new URL("/login", request.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  // If no redirect is needed, return the next response
   return NextResponse.next();
 };
 
 export const config = {
-  matcher: ["/", "/carts"],
+  matcher: ["/", "/carts", "/login"],
 };
