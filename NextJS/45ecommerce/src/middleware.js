b8ac3310b5b8c2f1941188e270ca33d59d314434
failedCoderrr <pankajkori2057@gmail.com>
2024-10-13 23:59:45 +0530

    ..

--- NextJS/45ecommerce/src/middleware.js
+++ NextJS/45ecommerce/src/middleware.js
@@ -2,11 +2,13 @@ import { NextResponse } from "next/server";
 
 export const middleware = (request) => {
   const path = request.nextUrl.pathname;
-  
-  
-  
+
+  console.log(path);
+
+  // You can return a NextResponse as needed, or just return null
+  return NextResponse.next();
 };
 
 export const config = {
-  matcher: "/:path*",
+  matcher: ["/", "/carts"],
 };
