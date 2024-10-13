c01ec9c4393431664fcada6aafd2f6be10d76a82
failedCoderrr <pankajkori2057@gmail.com>
2024-10-13 23:52:17 +0530

    middlware

--- NextJS/45ecommerce/src/middleware.js
+++ NextJS/45ecommerce/src/middleware.js
@@ -2,24 +2,9 @@ import { NextResponse } from "next/server";
 
 export const middleware = (request) => {
   const path = request.nextUrl.pathname;
-
-  const isPublicPath =
-    path === "/" ||
-    path === "/login" ||
-    path === "/signup" ||
-    path === "/verifyEmail";
-
-  const token = request.cookies.get("token")?.value || "";
-
-  if ((path === "/login" || path === "/signup") && token) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  return NextResponse.next();
+  
+  
+  
 };
 
 export const config = {
