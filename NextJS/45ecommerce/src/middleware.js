dfd0c736072317aa67f25b9c787bee0271f36170
failedCoderrr <pankajkori2057@gmail.com>
2024-10-14 23:07:27 +0530

    middleware

--- NextJS/45ecommerce/src/middleware.js
+++ NextJS/45ecommerce/src/middleware.js
@@ -4,21 +4,14 @@ export const middleware = (request) => {
   const token = request.cookies.get("token");
   const path = request.nextUrl.pathname;
 
-  console.log("Token:", token);
-  console.log("Path:", path);
-
-  // Check if the user is trying to access the /carts page
   if (path === "/carts") {
     if (token?.value) {
-      // Token exists, allow access to /carts
       return NextResponse.next();
     } else {
-      // Token doesn't exist, redirect to login
       return NextResponse.redirect(new URL("/login", request.url));
     }
   }
 
-  // If no redirect is needed, return the next response
   return NextResponse.next();
 };
 
