9fe84cb4c7fb0b28acf043e6c4e2bea7ac8aac45
failedCoderrr <pankajkori2057@gmail.com>
2024-10-14 21:00:39 +0530

    middleware

--- NextJS/45ecommerce/src/middleware.js
+++ NextJS/45ecommerce/src/middleware.js
@@ -1,14 +1,20 @@
 import { NextResponse } from "next/server";
 
 export const middleware = (request) => {
+  const user = JSON.stringify(localStorage.getItem("user"));
   const path = request.nextUrl.pathname;
 
   console.log(`Path: ${path}`);
 
   if (path === "/carts") {
     // keep a condition for the login state to be maintained if the user is login direct user to carts else redirect to login
-    const loginUrl = new URL("/login", request.url);
-    return NextResponse.redirect(loginUrl);
+    if (user.loggedIn == true) {
+      const cartUrl = new URL("/carts", request.url);
+      return NextResponse.redirect(cartUrl);
+    } else {
+      const loginUrl = new URL("/login", request.url);
+      return NextResponse.redirect(loginUrl);
+    }
   }
 
   // If no redirect is needed, return the next response
