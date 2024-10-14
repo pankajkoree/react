ddcc6094a10a8ee190f39db22da2f6da1556a889
failedCoderrr <pankajkori2057@gmail.com>
2024-10-14 23:33:25 +0530

    Cart

--- NextJS/45ecommerce/src/app/carts/page.jsx
+++ NextJS/45ecommerce/src/app/carts/page.jsx
@@ -1,10 +1,57 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+
+const getProductsData = async () => {
+  try {
+    let response = await fetch(
+      `http://localhost:3000/api/products/getProducts`
+    );
+    if (!response.ok) {
+      toast.error("failed to fetch...");
+    }
+    response = await response.json();
+    return response;
+  } catch (error) {
+    toast.error(error);
+    return false;
+  }
+};
+
 const Cart = () => {
+  const [specificProduct, setSpecificProduct] = useState({});
+
+  const { isLoading, error, data, isFetching } = useQuery({
+    queryKey: ["getProducts"],
+    queryFn: getProductsData,
+  });
+
   const cartItem = localStorage.getItem("cart");
-  
-  console.log(cartItem);
-  return <h1>Cart Page</h1>;
+
+  useEffect(() => {
+    if (data && cartItem) {
+      const foundProduct = data.results.find(
+        (product) => product._id === cartItem
+      );
+      if (foundProduct) {
+        setSpecificProduct(foundProduct);
+      }
+    }
+  }, [data, cartItem]);
+
+  console.log(specificProduct);
+
+  if (isFetching) return "fetching the data";
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "Error occurred: " + error.message;
+  return (
+    <div>
+      <h1>Cart</h1>
+    </div>
+  );
 };
 
 export default Cart;
