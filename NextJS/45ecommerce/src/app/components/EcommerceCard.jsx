564120747c0588435abcca00b2f37500f199ae7a
failedCoderrr <pankajkori2057@gmail.com>
2024-10-18 19:55:47 +0530

    Userview

--- NextJS/45ecommerce/src/app/components/EcommerceCard.jsx
+++ NextJS/45ecommerce/src/app/components/EcommerceCard.jsx
@@ -4,49 +4,28 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
-const EcommerceCard = ({ id, src, alt, title, price, rating }) => {
+const EcommerceCard = ({
+  id,
+  src,
+  alt,
+  title,
+  price,
+  rating,
+  addToCart,
+  addToBuyNow,
+}) => {
   const router = useRouter();
   let floorRating = Math.floor(rating);
-  let productIds = [];
-  let buyProductId = [];
-
-  const addedToCart = () => {
-    productIds.push(id);
-
-    localStorage.setItem("cart", productIds);
-
-    toast.success("added to cart", {
-      style: {
-        border: "1px solid purple",
-        padding: "8px",
-        color: "#ffffff",
-        background: "#19f57c",
-        fontSize: "16px",
-      },
-      iconTheme: {
-        primary: "#3177ff",
-        secondary: "#FFFAEE",
-      },
-    });
-  };
-
-  const addedToBuyNow = () => {
-    buyProductId.push(id);
-
-    localStorage.setItem("buyProduct", buyProductId);
-
-    router.push("/buyNow");
-  };
 
   const gotoProduct = () => {
     router.push(`/product/${id}`);
   };
+
   return (
     <div
-      className="border rounded-lg p-4 shadow-md shadow-indigo-400 dark:shadow-slate-400 dark:shadow-md dark:hover:shadow-lg dark:bg-silver-800 hover:shadow-lg transition-transform duration-200 ease-in-out hover:shadow-green-400 hover:scale-105 overflow-hidden dark:hover:shadow-green-500"
+      className="border rounded-lg p-4 shadow-md hover:shadow-lg transition-transform duration-200 ease-in-out hover:scale-105"
       onClick={gotoProduct}
     >
-      {/* Product thumbnail */}
       <div className="h-48 w-full mb-4">
         <Image
           loading="lazy"
@@ -54,28 +33,21 @@ const EcommerceCard = ({ id, src, alt, title, price, rating }) => {
           alt={alt}
           width={1920}
           height={1080}
-          className="object-contain h-full w-full transition-transform duration-200 ease-in-out hover:scale-125 drop-shadow-xl"
-          style={{
-            filter: "drop-shadow(0 2px 4px rgba(255, 255, 255, 0.3))",
-          }}
+          className="object-contain h-full w-full transition-transform hover:scale-125 drop-shadow-xl"
         />
       </div>
-
-      {/* Title */}
       <h2 className="text-lg font-semibold">{title}</h2>
-      {/* Price and rating */}
       <div className="flex justify-between mt-2">
         <p className="text-xl font-bold">${price}</p>
         <p className="text-sm text-gray-500">{"⭐".repeat(floorRating)}</p>
       </div>
-      {/* Buttons */}
       <div className="flex justify-between mt-4">
         <Button
           variant="carting"
           className="text-xl hover:text-white"
           onClick={(e) => {
             e.stopPropagation();
-            addedToCart();
+            addToCart(id);
           }}
         >
           Add to cart
@@ -85,7 +57,7 @@ const EcommerceCard = ({ id, src, alt, title, price, rating }) => {
           className="text-xl hover:text-white"
           onClick={(e) => {
             e.stopPropagation();
-            addedToBuyNow();
+            addToBuyNow(id);
           }}
         >
           Buy now
