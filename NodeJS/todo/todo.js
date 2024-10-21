c7f4cf0213f797547257149fafca8cbc14c19438
failedCoderrr <pankajkori2057@gmail.com>
2024-10-22 00:10:22 +0530

    file system in node js

--- NodeJS/todo/todo.js
+++ NodeJS/todo/todo.js
@@ -5,9 +5,7 @@ const { argv } = require("process");
 
 const filePath = "./tasks.json";
 
-const loadTasks = () => {};
-
-const addTask = (task) => {
+const loadTasks = () => {
   try {
     const dataBuffer = fs.readFileSync(filePath);
     const dataJSON = dataBuffer.toString();
@@ -16,13 +14,30 @@ const addTask = (task) => {
     return [];
   }
 };
+
+const saveTasks = (tasks) => {
+  const dataJSON = JSON.stringify(tasks);
+  fs.writeFileSync(filePath, dataJSON);
+};
+
+const addTask = (task) => {
+  const tasks = loadTasks();
+  tasks.push({ task });
+  saveTasks(tasks);
+  console.log("Task added", task);
+};
+
+const listTasks = () => {
+  const tasks = loadTasks();
+  tasks.forEach((task, index) => console.log(`${index + 1} - ${task.task}`));
+};
 // argv = argument value, which is an array
 const command = process.argv[2];
 const argument = process.argv[3];
 
 if (command === "add") {
   addTask(argument);
-} else if (command === list) {
+} else if (command === "list") {
   listTasks();
 } else if (command === "remove") {
   removeTask(parseInt(argument));
