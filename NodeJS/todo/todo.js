0bf2c892df2ed65e60e63fca261a29db89e5ac37
failedCoderrr <pankajkori2057@gmail.com>
2024-10-21 23:54:40 +0530

    node js file system

--- NodeJS/todo/todo.js
+++ NodeJS/todo/todo.js
@@ -1,9 +1,19 @@
 // file system as fs
 
 const fs = require("fs");
+const { argv } = require("process");
 
 const filePath = "./tasks.json";
+// argv = argument value, which is an array
+const command = process.argv[2];
+const argument = process.argv[3];
 
 if (command === "add") {
-  addTask();
+  addTask(argument);
+} else if (command === list) {
+  listTasks();
+} else if (command === "remove") {
+  removeTask(parseInt(argument));
+} else {
+  console.log("command not found...");
 }
