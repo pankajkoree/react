72dc6b7a53c1693ec9fe4508f157d87b88578c74
failedCoderrr <pankajkori2057@gmail.com>
2024-10-22 23:44:18 +0530

    logger in node js

---
+++ NodeJS/03_logger/logger.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const os = require("os");
+
+// its a class
+const EventEmitter = require("events");
+
+class Logger extends EventEmitter {
+  log(message) {
+    this.emit("message", { message });
+  }
+}
+
+const logger = new Logger();
+
+const logFile = "eventlog.txt";
+
+const logTofile = (event) => {
+  const logMessage = `${new Date().toLocaleString()} - ${event.message} \n`;
+
+  fs.appendFileSync(logFile, logMessage);
+};
+
+logger.on("message", logTofile);
+
+setInterval(() => {
+  const memoryUsage = (os.freemem() / os.totalmem()) * 100;
+
+  logger.log(`Current memory usage: ${memoryUsage.toFixed(2)}`);
+}, 5000);
+
+logger.log("Application started");
+logger.log("Application event occured");
