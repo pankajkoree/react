ea57adc6646686352e1cdae2e88471b8851630f6
failedCoderrr <pankajkori2057@gmail.com>
2024-10-22 13:27:55 +0530

    file system

--- NodeJS/01_todo/todo.js
+++ NodeJS/01_todo/todo.js
@@ -27,21 +27,27 @@ const addTask = (task) => {
   saveTasks(tasks);
   console.log("Task added", task);
 };
+const removeTask = () => {
+  const tasks = loadTasks();
+  tasks.pop();
+  saveTasks(tasks);
+  console.log("Task removed");
+};
 
 const listTasks = () => {
   const tasks = loadTasks();
   tasks.forEach((task, index) => console.log(`${index + 1} - ${task.task}`));
 };
 // argv = argument value, which is an array
-const command = process.argv[2];
-const argument = process.argv[3];
+const command = argv[2];
+const argument = argv[3];
 
 if (command === "add") {
   addTask(argument);
 } else if (command === "list") {
   listTasks();
 } else if (command === "remove") {
-  removeTask(parseInt(argument));
+  removeTask();
 } else {
   console.log("command not found...");
 }
