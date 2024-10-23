88bc77448e44f28a9a2e9bf6a7531fec4741a7e5
failedCoderrr <pankajkori2057@gmail.com>
2024-10-23 23:17:05 +0530

    express js

--- NodeJS/05_expressJS/index.js
+++ NodeJS/05_expressJS/index.js
@@ -7,6 +7,17 @@ app.get("/", (req, res) => {
   res.send("express js server");
 });
 
+app.get("/pankaj", (req, res) => {
+  res.send("welcome pankaj");
+});
+app.get("/world", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/github", (req, res) => {
+  res.send("github.com/pankajkoree");
+});
+
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
 });
