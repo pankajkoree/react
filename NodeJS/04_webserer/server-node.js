39b02f09ea2952ae0f575824ea5483eeaf3bb482
failedCoderrr <pankajkori2057@gmail.com>
2024-10-23 21:51:40 +0530

    node server

--- NodeJS/04_webserer/server-node.js
+++ NodeJS/04_webserer/server-node.js
@@ -4,9 +4,19 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("node server");
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("node server");
+  } else if (req.url === "/pankaj") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("pankaj server");
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("404 server not found");
+  }
 });
 
 // listening to the server
