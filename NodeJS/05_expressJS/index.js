4fec9498308e85115248e2e39da8cbda96db0c01
failedCoderrr <pankajkori2057@gmail.com>
2024-10-24 21:48:02 +0530

    contact mgmt in express

--- NodeJS/05_expressJS/index.js
+++ NodeJS/05_expressJS/index.js
@@ -3,20 +3,25 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("express js server");
-});
+// testing the routes
+// app.get("/", (req, res) => {
+//   res.send("express js server");
+// });
 
-app.get("/pankaj", (req, res) => {
-  res.send("welcome pankaj");
-});
-app.get("/world", (req, res) => {
-  res.send("hello world");
-});
+// app.get("/pankaj", (req, res) => {
+//   res.send("welcome pankaj");
+// });
+// app.get("/world", (req, res) => {
+//   res.send("hello world");
+// });
 
-app.get("/github", (req, res) => {
-  res.send("github.com/pankajkoree");
-});
+// app.get("/github", (req, res) => {
+//   res.send("github.com/pankajkoree");
+// });
+
+
+
+// making the route for contact management
 
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
