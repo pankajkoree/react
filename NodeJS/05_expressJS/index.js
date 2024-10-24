c1c9db5b02594b3be3e16c58c8e501761b419614
failedCoderrr <pankajkori2057@gmail.com>
2024-10-24 23:13:03 +0530

    contact mgmt

--- NodeJS/05_expressJS/index.js
+++ NodeJS/05_expressJS/index.js
@@ -3,39 +3,76 @@ import express from "express";
 const app = express();
 const port = 4000;
 
-// testing the routes
-// app.get("/", (req, res) => {
-//   res.send("express js server");
-// });
-
-// app.get("/pankaj", (req, res) => {
-//   res.send("welcome pankaj");
-// });
-// app.get("/world", (req, res) => {
-//   res.send("hello world");
-// });
-
-// app.get("/github", (req, res) => {
-//   res.send("github.com/pankajkoree");
-// });
-
-// making the route for contact management
 app.use(express.json());
 const contacts = [];
-let contactId = 1;
+let id = 1;
 
-// add api
+// Add contact API
 app.post("/add", (req, res) => {
   const { contactName, contactNumber } = req.body;
-  const newContact = { contactId: contactId++, contactName, contactNumber };
+  const newContact = { id: id++, contactName, contactNumber };
   contacts.push(newContact);
 
   res.status(200).send(newContact);
 });
 
-// get all contact api
+// Get all contacts API
 app.get("/getContact", (req, res) => {
   res.status(200).send(contacts);
+});
+
+// Get contact by ID API
+app.get("/getContact/:id", (req, res) => {
+  const contact = contacts.find(
+    (contact) => contact.id === parseInt(req.params.id)
+  );
+  if (!contact) {
+    return res.status(404).send("Contact not found");
+  }
+
+  res.status(200).send(contact);
+});
+
+// Update contact by ID API
+app.put("/getContact/:id", (req, res) => {
+  const contact = contacts.find(
+    (contact) => contact.id === parseInt(req.params.id)
+  );
+
+  if (!contact) {
+    return res.status(404).send("Contact not found");
+  }
+
+  const { contactName, contactNumber } = req.body;
+
+  // Validate if both contactName and contactNumber are present
+  if (!contactName || !contactNumber) {
+    return res
+      .status(400)
+      .send("Both contact name and contact number are required");
+  }
+
+  contact.contactName = contactName;
+  contact.contactNumber = contactNumber;
+
+  res.status(200).send({
+    message: "Contact updated",
+    updatedContact: contact,
+  });
+});
+
+// Delete contact by ID API
+app.delete("/getContact/:id", (req, res) => {
+  const index = contacts.findIndex(
+    (contact) => contact.id === parseInt(req.params.id)
+  );
+
+  if (index === -1) {
+    return res.status(404).send("Contact not found");
+  }
+
+  contacts.splice(index, 1);
+  res.status(200).send("Contact deleted");
 });
 
 app.listen(port, () => {
