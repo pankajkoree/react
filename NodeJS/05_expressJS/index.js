d71dc97cebbf3952e106a341bbfb0c58feda4113
failedCoderrr <pankajkori2057@gmail.com>
2024-10-24 22:44:32 +0530

    contact mgmt

--- NodeJS/05_expressJS/index.js
+++ NodeJS/05_expressJS/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // testing the routes
 // app.get("/", (req, res) => {
@@ -19,9 +19,24 @@ const port = 3000;
 //   res.send("github.com/pankajkoree");
 // });
 
-
-
 // making the route for contact management
+app.use(express.json());
+const contacts = [];
+let contactId = 1;
+
+// add api
+app.post("/add", (req, res) => {
+  const { contactName, contactNumber } = req.body;
+  const newContact = { contactId: contactId++, contactName, contactNumber };
+  contacts.push(newContact);
+
+  res.status(200).send(newContact);
+});
+
+// get all contact api
+app.get("/getContact", (req, res) => {
+  res.status(200).send(contacts);
+});
 
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
