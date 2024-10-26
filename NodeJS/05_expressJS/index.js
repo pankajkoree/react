7d3207d15d898195c595fd99a478efb788de9bf6
failedCoderrr <pankajkori2057@gmail.com>
2024-10-26 22:46:10 +0530

    waston morgan for logging

--- NodeJS/05_expressJS/index.js
+++ NodeJS/05_expressJS/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+
+// import the module for morgan and logger file
 import logger from "./logger";
 import morgan from "morgan";
 
@@ -6,6 +8,26 @@ const app = express();
 const port = 4000;
 
 app.use(express.json());
+
+// set morganFormat
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 const contacts = [];
 let id = 1;
 
