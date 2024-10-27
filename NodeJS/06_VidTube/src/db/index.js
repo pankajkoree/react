c4a4423b28c2db3950477e4c04d90bbc0647e2c3
failedCoderrr <pankajkori2057@gmail.com>
2024-10-27 22:28:18 +0530

    db index.js

--- NodeJS/06_VidTube/src/db/index.js
+++ NodeJS/06_VidTube/src/db/index.js
@@ -3,7 +3,12 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    const connectionInstance = await mongoose.connect(
+      `process.env.MONGODB_URI/${DB_NAME}`
+    );
+    console.log(
+      `MongoDB connected successfully with the host : ${connectionInstance.connection.host}`
+    );
   } catch (error) {
     console.log("MongoDB connection error : ", error);
     process.exit(1);
