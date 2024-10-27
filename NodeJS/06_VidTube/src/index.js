7bbe378addc19f7d8fd287afaf3cebcb9f9c4407
failedCoderrr <pankajkori2057@gmail.com>
2024-10-27 18:47:52 +0530

    implemented logger

--- NodeJS/06_VidTube/src/index.js
+++ NodeJS/06_VidTube/src/index.js
@@ -1,1 +1,27 @@
+import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+
 console.log("hello");
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
