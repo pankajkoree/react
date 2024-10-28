2ecc92462376d43c28b2cc144947ec036b787d8d
failedCoderrr <pankajkori2057@gmail.com>
2024-10-28 14:30:24 +0530

    usermodel

--- NodeJS/06_VidTube/src/models/user.models.js
+++ NodeJS/06_VidTube/src/models/user.models.js
@@ -11,8 +11,9 @@ refreshtoken String
 createdAt Date
 updatedAt Date
 */
-
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -60,5 +61,43 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// generate the access token
+userSchema.methods.generateAccessToken = function () {
+  //short lived access token
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+// generate refresh token
+userSchema.methods.generateRefreshToken = function () {
+  //short lived access token
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
