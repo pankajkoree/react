3a914ddb204abbe6e4ac40028d330cc16c0470a6
failedCoderrr <pankajkori2057@gmail.com>
2024-10-30 21:32:54 +0530

    user controller

--- NodeJS/06_VidTube/src/controllers/user.controller.js
+++ NodeJS/06_VidTube/src/controllers/user.controller.js
@@ -57,14 +57,14 @@ const registerUser = asyncHandler(async (req, res, next) => {
     process.cwd(),
     "public",
     "temp",
-    req.files?.coverImage?.[0]?.filename || "",
+    req.files?.coverImage?.[0]?.filename || ""
   );
 
   const avatarLocalPath = path.join(
     process.cwd(),
     "public",
     "temp",
-    req.files?.avatar?.[0]?.filename || "",
+    req.files?.avatar?.[0]?.filename || ""
   );
 
   console.log("Cover image local path:", coverLocalPath);
@@ -112,7 +112,7 @@ const registerUser = asyncHandler(async (req, res, next) => {
     });
 
     const createdUser = await User.findById(user._id).select(
-      "-password -refreshToken",
+      "-password -refreshToken"
     );
     if (!createdUser) {
       throw new ApiError(500, "User creation failed");
@@ -134,7 +134,7 @@ const registerUser = asyncHandler(async (req, res, next) => {
 
     throw new ApiError(
       500,
-      "Something went wrong while registering a user and images were deleted",
+      "Something went wrong while registering a user and images were deleted"
     );
   }
 });
@@ -165,11 +165,11 @@ const loginUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, `password is incorrect`);
   }
   const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
-    user._id,
+    user._id
   );
 
   const loggedInUser = await User.findById(user._id).select(
-    "-password -refreshToken",
+    "-password -refreshToken"
   );
 
   const options = {
@@ -188,8 +188,8 @@ const loginUser = asyncHandler(async (req, res) => {
           accessToken,
           refreshToken,
         },
-        "User logged in successfully",
-      ),
+        "User logged in successfully"
+      )
     );
 });
 
@@ -203,7 +203,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   try {
     const decodedToken = jwt.verify(
       incomingRefreshToken,
-      process.env.REFRESH_TOKEN_SECRET,
+      process.env.REFRESH_TOKEN_SECRET
     );
     const user = await User.findById(decodedToken?._id);
     if (!user) {
@@ -232,8 +232,8 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
             accessToken,
             refreshToken: newRefreshToken,
           },
-          "Access token refreshed successfully",
-        ),
+          "Access token refreshed successfully"
+        )
       );
   } catch (error) {
     throw new ApiError(500, error);
@@ -241,22 +241,27 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  await User.findByIdAndUpdate(req.user._id, {
-    $set: {
-      refreshToken: undefined,
-
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
     },
     {
-      new: true
+      new: true,
     }
-  })
-
+  );
   const options = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
   };
 
-  return res.status(200).clearCookie("accessToken",options).clearCookie("refreshToken",options).json(new ApiResponse(200,{},"User logged out successfully"))
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
 });
 
 export { registerUser, loginUser, refreshAccessToken, logoutUser };
--- NodeJS/06_VidTube/src/routes/user.route.js
+++ NodeJS/06_VidTube/src/routes/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"; // Adjust path as necessary
+import verifyJWT from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
