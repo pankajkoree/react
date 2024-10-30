d6bec46ac0aec4f74d4e121056fd6a90056d1975
failedCoderrr <pankajkori2057@gmail.com>
2024-10-30 21:26:46 +0530

    user controller

--- NodeJS/06_VidTube/src/controllers/user.controller.js
+++ NodeJS/06_VidTube/src/controllers/user.controller.js
@@ -241,10 +241,22 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  await User
-    .findByIdAndUpdate
-    // need to comeback after middleware
-    ();
+  await User.findByIdAndUpdate(req.user._id, {
+    $set: {
+      refreshToken: undefined,
+
+    },
+    {
+      new: true
+    }
+  })
+
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  };
+
+  return res.status(200).clearCookie("accessToken",options).clearCookie("refreshToken",options).json(new ApiResponse(200,{},"User logged out successfully"))
 });
 
 export { registerUser, loginUser, refreshAccessToken, logoutUser };
