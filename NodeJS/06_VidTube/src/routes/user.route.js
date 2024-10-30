f21450be9455f3a43999dffacc1575620c98e49b
failedCoderrr <pankajkori2057@gmail.com>
2024-10-30 21:35:39 +0530

    user route

--- NodeJS/06_VidTube/src/routes/user.route.js
+++ NodeJS/06_VidTube/src/routes/user.route.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, logoutUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"; // Adjust path as necessary
-import verifyJWT from "../middlewares/auth.middleware.js"
+import verifyJWT from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 // Directly use `upload` middleware for handling file uploads
 router.route("/register").post(upload, registerUser);
+
+// secured routes
+router.routes("/logout").post(verifyJWT, logoutUser);
 
 export default router;
