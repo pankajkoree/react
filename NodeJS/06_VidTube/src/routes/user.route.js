3930a49c112ccff0b0d2f44f702d020063c931f3
failedCoderrr <pankajkori2057@gmail.com>
2024-10-30 23:05:36 +0530

    user route

--- NodeJS/06_VidTube/src/routes/user.route.js
+++ NodeJS/06_VidTube/src/routes/user.route.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { registerUser, logoutUser } from "../controllers/user.controller.js";
+import {
+  registerUser,
+  logoutUser,
+  loginUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  updateAccountDetails,
+  getCurrentUser,
+  getUserChannelProfile,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getWatchHistory,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import verifyJWT from "../middlewares/auth.middleware.js";
 
@@ -7,8 +19,21 @@ const router = Router();
 
 // Directly use `upload` middleware for handling file uploads
 router.route("/register").post(upload, registerUser);
+router.route("/login").post(loginUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 // secured routes
-router.routes("/logout").post(verifyJWT, logoutUser);
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/update-cover")
+  .patch(verifyJWT, upload.single("avatar"), updateUserCoverImage);
+router.route("/history").patch(verifyJWT, getWatchHistory);
 
 export default router;
