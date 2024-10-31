186615ac4645bf3c9eb541badb2fb96661b387b7
failedCoderrr <pankajkori2057@gmail.com>
2024-10-31 15:31:46 +0530

    arrays

--- JS/DSA/DSA/arrays/01_arrays.js
+++ JS/DSA/DSA/arrays/01_arrays.js
@@ -12,14 +12,32 @@ class MyArray {
     return this.arrayLength;
   }
 
-  pop() {}
+  get(index) {
+    if (index > this.arrayLength) {
+      return `index is out of range`;
+    }
+    return this.arrayData[index - 1];
+  }
+
+  //   pop() {
+  //     if (this.arrayLength <= 0) {
+  //       return `underflow`;
+  //     }
+  //     this.arrayLength = this.arrayLength - 1;
+  //     return (this.arrayData[this.arrayLength]);
+  //   }
 }
 
 const arrayObject = new MyArray();
 
+// push
 arrayObject.push(2);
 arrayObject.push(5);
 arrayObject.push(9);
 arrayObject.push(7);
-
 console.log(arrayObject.arrayData);
+console.log(arrayObject.arrayLength);
+
+// get
+console.log(arrayObject.get(5));
+console.log(arrayObject.get(4));
