cd58a68549df2c6babb22e112c52866f07065175
failedCoderrr <pankajkori2057@gmail.com>
2024-10-31 12:30:33 +0530

    problem 1

---
+++ JS/DSA/DSA/01_problem.js
@@ -0,0 +1,15 @@
+// problem 1: in a record of student, find the specific student
+
+// data structures
+const studentRecords = ["aftab", "risek", "selera", "kashif"];
+
+// algorithm
+const findStudent = (studentRecords, studentName) => {
+  for (let i = 0; i < studentRecords.length; i++) {
+    if (studentRecords[i] === studentName) {
+      console.log(`Found ${studentName}`);
+    }
+  }
+};
+
+findStudent(studentRecords, "risek");
