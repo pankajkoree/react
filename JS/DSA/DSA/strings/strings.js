6a0c06d6327442ece8beee1a57b39babef2fa1bf
failedCoderrr <pankajkori2057@gmail.com>
2024-11-02 13:46:57 +0530

    strings

--- JS/DSA/DSA/strings/strings.js
+++ JS/DSA/DSA/strings/strings.js
@@ -13,3 +13,22 @@ function reverseString(originalString) {
 }
 
 console.log(reverseString(originalString));
+
+// problem 2: checking the palindrome of the string
+
+function stringPalindromeChecker(string) {
+  let reversedString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+
+  if (string === reversedString) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(stringPalindromeChecker("apple"));
+console.log(stringPalindromeChecker("eye"));
+console.log(stringPalindromeChecker("dribble"));
