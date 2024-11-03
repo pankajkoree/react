daf4675eaef4697244cf9fae5b627eed3078620b
failedCoderrr <pankajkori2057@gmail.com>
2024-11-03 12:22:11 +0530

    http

--- NodeJS/07_http/index.js
+++ NodeJS/07_http/index.js
@@ -2,9 +2,21 @@ import http from "http";
 import fs from "fs";
 
 const server = http.createServer((req, res) => {
-  const log = `${new Date()} : request \n`;
+  const log = `${new Date()} : request on url ${req.url} \n`;
   fs.appendFile("log.txt", log, (err, data) => {
-    res.end("request added to log");
+    switch (req.url) {
+      case "/":
+        res.end("home page");
+        break;
+      case "/about":
+        res.end("about us");
+        break;
+      case "/contact":
+        res.end("contact page");
+        break;
+      default:
+        res.end("404 not found");
+    }
   });
 });
 
