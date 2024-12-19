1e796a3e836d32ad95608b2b1e6f5f2f5444e567
pankajkoree <pankajkori2057@gmail.com>
2024-12-19 17:58:31 +0530

    models for user in MVC

---
+++ NodeJS/08_project1/models/user.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+// user schema
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    jobTitle: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const user = mongoose.model("user", userSchema);
