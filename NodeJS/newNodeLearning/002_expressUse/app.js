34921866faa7686c46cb123b49114979fe1750b5
pankajkoree <pankajkori2057@gmail.com>
2025-09-28 09:43:38 +0545

    learning routes

--- NodeJS/newNodeLearning/002_expressUse/app.js
+++ NodeJS/newNodeLearning/002_expressUse/app.js
@@ -1,16 +1,20 @@
 const express = require("express");
+const path = require("path");
+const rootDirectory = require("./utils/pathUtil");
 
 const app = express();
 
 const PORT = 3000;
 
 const homeRouter = require("./routes/homeRouter");
+const contactRouter = require("./routes/contactRouter");
 
 app.use(homeRouter);
-
-app.get("/contact-us", (req, res, next) => {});
-
-app.post("/contact-us", (req, res, next) => {});
+app.use(contactRouter);
+app.use((req, res, next) => {
+  console.log(`Page not found, URL: ${req.url} Method: ${req.method}`);
+  res.sendFile(path.join(rootDirectory, "views", "notFound.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on address http://localhost:${PORT}`);
--- NodeJS/newNodeLearning/002_expressUse/routes/contactRouter.js
+++ NodeJS/newNodeLearning/002_expressUse/routes/contactRouter.js
@@ -1,2 +1,22 @@
-const path = require('path')
-const 
+const path = require("path");
+const express = require("express");
+const rootDirectory = require("../utils/pathUtil");
+
+const contactRouter = express.Router();
+
+contactRouter.get("/contact-us", (req, res, next) => {
+  console.log(
+    `Contact us router page with get method as : URL: ${req.url} Method: ${req.method} `
+  );
+  res.sendFile(path.join(rootDirectory, "views", "contactUs.html"));
+});
+
+contactRouter.post("/contact-us", (req, res, next) => {
+  console.log(
+    `Contact us router page with post method as : URL: ${req.url} Method: ${req.method} `
+  );
+
+  res.sendFile(path.join(rootDirectory, "views", "submittedDetails.html"));
+});
+
+module.exports = contactRouter;
